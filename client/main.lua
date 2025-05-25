f853ff798c5b7004a4ad70e80f659230c3ccb268
Lenix <149199196+lenix-x@users.noreply.github.com>
2025-05-25 20:43:25 +0100

    Add files via upload

--- client/main.lua
+++ client/main.lua
@@ -1,16 +1,18 @@
 local longVoiceRange = Option.Range
-local micFilter, isMicActive, isToggleOn = false, false
-local playerPed = PlayerPedId()
-local vehicle = GetVehiclePedIsIn(playerPed, false)
-local vehicleClass = GetVehicleClass(vehicle)
-local vehicleModel = GetEntityModel(vehicle)
-
+local micFilter, isMicActive, isToggleOn = false, false, false
+local canUseMic = true
 local VehicleModels = Option.VehicleModels
 local VehicleClasses = Option.VehicleClass
 
 function isEmergencyVehicle()
+    local playerPed = PlayerPedId()
     if IsPedInAnyVehicle(playerPed, false) then
+        local vehicle = GetVehiclePedIsIn(playerPed, false)
+        local vehicleClass = GetVehicleClass(vehicle)
+        local vehicleModel = GetEntityModel(vehicle)
+        
         if VehicleModels[vehicleModel] then
+            canUseMic = true
             return true
         end
         return VehicleClasses[vehicleClass] or false
@@ -37,9 +39,34 @@ function removeMicFilter()
     MumbleSetSubmixForServerId(PlayerId(), -1)
 end
 
+function deactivateMic()
+    removeMicFilter()
+    exports["pma-voice"]:clearProximityOverride()
+    isToggleOn = false
+    isMicActive = false
+    exports["interaction"]:showInteraction(nil, 'Deactivated')
+    Wait(1000)
+    exports["interaction"]:hideInteraction()
+end
+
+function vehicleCheckLoop()
+    CreateThread(function()
+        while isToggleOn do
+            Wait(500)
+            if not isEmergencyVehicle() then
+                canUseMic = false
+                exports['qb-core']:Notify('You left the emergency vehicle, mic turned off!', 'warning', 7500)
+                deactivateMic()
+                break
+            end
+        end
+    end)
+end
+
 CreateThread(function()
     createMicFilter()
 end)
+
 RegisterCommand(Option.Command, function()
     TriggerEvent('patrolmic:client:toggle')
 end, false)
@@ -47,34 +74,24 @@ end, false)
 RegisterKeyMapping(Option.Command, Option.Description, 'keyboard', Option.Key)
 
 RegisterNetEvent('patrolmic:client:toggle', function()
-    if isEmergencyVehicle() then
+    print('Toggling Patrol Mic')
+    if not isEmergencyVehicle() then
+        exports['qb-core']:Notify('You must be in an emergency vehicle to use the patrol mic!', 'error', 5000)
+        return
+    end
+    
+    if canUseMic then
         isMicActive = not isMicActive
         if isMicActive then
             applyMicFilter()
             exports["pma-voice"]:overrideProximityRange(longVoiceRange, true)
             isToggleOn = true
-            exports["interaction"]:showInteraction('J', 'Actived')
+            exports["interaction"]:showInteraction('J', 'Activated')
+            vehicleCheckLoop()
         else
-            removeMicFilter()
-            exports["pma-voice"]:clearProximityOverride()
-            isToggleOn = false
-            exports["interaction"]:showInteraction(nil, 'Deactivated')
-            Wait(1000)
-            exports["interaction"]:hideInteraction()
+            deactivateMic()
         end
-    end
-end)
-
-CreateThread(function()
-    while true do
-        Wait(0)
-        if isToggleOn and not IsPedInAnyVehicle(playerPed, false) then
-            exports["pma-voice"]:clearProximityOverride()
-            exports['qb-core']:Notify('You forget to turn off the mic, it is off now!', 'warning', 7500)
-            exports["interaction"]:showInteraction(nil, 'Deactivated')
-            Wait(1000)
-            exports["interaction"]:hideInteraction()
-            isMicActive = false
-        end
+    else
+        exports['qb-core']:Notify('Patrol mic is not available right now!', 'error', 3000)
     end
 end)
--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,16 +2,13 @@ fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
 
-author 'https://forum.cfx.re/u/lenix_x/summary'
+author 'Lenix'
 description 'An enhanced script that make the game more realistic when a patrol hit the mic button to speak to the outside of the car'
 version '1.0.0'
 
 client_script 'client/*.lua'
 
-shared_scripts {
-    'config/*.lua',
-    'shared/*.lua'
-}
+shared_script  'config/*.lua',
 
 server_script 'server/*.lua'
 
