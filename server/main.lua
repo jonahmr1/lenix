2f2c4b3ed089cc19e007f63dd7606b14c1244c1b
Lenix <149199196+LenixDev@users.noreply.github.com>
2025-06-20 23:42:21 +0100

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -1,3 +1,16 @@
+CreateThread(function()
+   print("  ____              _                      _        ")
+   print(" | __ )   _   _    | |       ___   _ __   (_) __  __")
+   print(" |  _ \\  | | | |   | |     / _ \\  | '_ \\  | | \\\\/ /")
+   print(" | |_) | | |_| |   | |___  | __/  | | | | | |  >  < ")
+   print(" |____/   \\__, |   |_____| \\___|  |_| |_| |_| /_/\\\\ ")
+   print("          |___/                                     ")
+end)
+
+if GetCurrentResourceName() ~= "trplr_patrolmic" then
+    return print("^6Changing the resource's name wont't let the resource start, ^1" .. GetCurrentResourceName() .. "^0 > ^2 trplr_patrolmic ^7")
+end
+
 QBCore = exports['qb-core']:GetCoreObject()
 
 Config = {}
