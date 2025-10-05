4fdfc09e74f9849ec203ef74fa1114bb0398b7c0
Lenix <lenixdev@users.noreply.github.com>
2025-10-05 20:40:10 +0100

    source code

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,8 +1,9 @@
 fx_version 'cerulean'
 game 'gta5'
-author 'Trippler Hub'
 lua54 'yes'
 
-client_script 'client.lua',
+author 'Trippler Hub'
+
+client_script 'client.lua'
 ui_page 'web/build/index.html'
 file 'web/**'
