1a4c51f8da80d1ae42db243fcc1e8037d95ff41e
Lenix <149199196+LenixDev@users.noreply.github.com>
2025-10-07 19:07:37 +0100

    Hub to Scripts

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,11 +1,13 @@
 fx_version 'cerulean'
 game 'gta5'
+lua54 'yes'
 dependencies {
   'qb-core',
   'qb-menu',
   'qb-target'
 }
 
+author 'Trippler Scripts'
 description 'patrolvehicle - A QBCore resource for managing patrol vehicles in FiveM.'
 version '1.0'
 
