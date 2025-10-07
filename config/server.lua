52032612803024ce192d452f377217c27d7bbbb7
Lenix <lenixdev@users.noreply.github.com>
2025-10-07 09:20:16 +0100

    Commit

--- config/server.lua
+++ config/server.lua
@@ -1,13 +1,13 @@
 Config = {}
 
-Config.LogEnabled = false -- Want to use the discord webhook? if not set it to false
-Config.Webhook = "change_me" --Discord Webhook
-Config.BotName = "change_me" --Bot Name
-Config.BotImage = "change_me" --Bot Image
+Config.LogEnabled = false
+Config.Webhook = ""
+Config.BotName = ""
+Config.BotImage = ""
 
-Config.EmbedColor = "change_me" --Embed Color
-Config.EmbedTitle = "change_me" --Embed Title
-Config.DiscordLink = "change_me" --Discord Link
-Config.EmbedFooterName = "change_me" --Embed Footer Name
-Config.EmbedFooterImage = "change_me" --Embed Footer Image
-Config.EmbedThumbainlImage = "change_me" --Embed Thumbnail Image
+Config.EmbedColor = ""
+Config.EmbedTitle = ""
+Config.DiscordLink = ""
+Config.EmbedFooterName = ""
+Config.EmbedFooterImage = ""
+Config.EmbedThumbainlImage = ""
