2aeb65b49a0ae5d61671a949b788b88412ca37f9
Lenix <lenix01x@gmail.com>
2025-10-22 22:49:32 +0100

    Commit

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,13 +2,13 @@ fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
 
-author 'Trippler Scripts'
+author 'Trippler'
+docs 'https://docs.trippler.store'
 
 client_scripts {
   'client/bridge.js',
   'client/build/main.js',
 }
-
 ui_page 'web/index.html'
 files {
   'web/build/main.js',
