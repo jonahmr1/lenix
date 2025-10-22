038e46c39b64099b058cbff3994994d9602364fd
Lenix <lenix01x@gmail.com>
2025-10-22 22:50:01 +0100

    Commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# Documentation
-- https://docs.trippler.store
--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,9 +1,10 @@
 fx_version 'cerulean'
 game 'gta5'
+lua54 'yes'
 
 author 'Trippler'
+docs 'https://docs.trippler.store'
 
 client_script 'client/main.js'
-
 ui_page 'web/index.html'
 file 'web/**'
