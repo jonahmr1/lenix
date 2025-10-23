37da502657e1cbab250164b24070c85d22775bf3
Lenix <lenix01x@gmail.com>
2025-10-23 16:17:21 +0100

    Commit

--- client/main.js
+++ client/main.js
@@ -1,16 +1,20 @@
-exports('show', function(key, text) {
-  let localKey
-  localKey = key != undefined || key != '' ? key : undefined
+RegisterNuiCallback('nuiReady', function(data, cb) {
+  cb(true);
+  
+  exports('show', function(key, text) {
+    let localKey
+    localKey = key != undefined || key != '' ? key : undefined
 
-  SendNuiMessage(JSON.stringify({
-    action: "show",
-    key: localKey,
-    text: text
-  }))
-})
+    SendNuiMessage(JSON.stringify({
+      action: "show",
+      key: localKey,
+      text: text
+    }))
+  })
 
-exports("hide", function() {
-  SendNuiMessage(JSON.stringify({
-    action: "hide"
-  }))
+  exports("hide", function() {
+    SendNuiMessage(JSON.stringify({
+      action: "hide"
+    }))
+  })
 })
--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,8 @@ lua54 'yes'
 
 author 'Trippler'
 docs 'https://docs.trippler.store'
+version '1.0.0'
 
-client_script 'client/main.js'
+client_script 'client/build/main.js'
 ui_page 'web/index.html'
 file 'web/**'
