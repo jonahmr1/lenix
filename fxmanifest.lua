d9b77f14d1ead5064533d9a8bc90d47298929639
Lenix <lenix01x@gmail.com>
2025-10-25 11:54:21 +0100

    bumped the version & added a new dependency

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,11 +5,13 @@ dependencies {
   'qbx_core',
   'ox_inventory',
   'ox_target',
-  'ox_lib'
+  'ox_lib',
+  'tr_kit'
 }
 
 author 'Trippler'
 docs 'https://docs.trippler.store'
+version '1.0.0'
 
 shared_scripts {
   '@ox_lib/init.lua',
@@ -25,7 +27,7 @@ client_scripts {
   'client/settings.lua',
   'client/main.lua',
 }
-ui_page 'web/build/index.html'
+ui_page 'web/index.html'
 file 'web/**'
 
 escrow_ignore {
