7266b6229ca87b6929249679356495d51870ab80
Lenix <lenix01x@gmail.com>
2025-10-25 19:59:55 +0100

    Update author and client script path in fxmanifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,10 +2,10 @@ fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
 
-author 'Trippler'
-docs 'https://docs.trippler.store'
+author 'https://lenix.dev'
 version '1.0.0'
 
-client_script 'client/build/main.js'
+client_script 'client/main.js'
 ui_page 'web/index.html'
+
 file 'web/**'
