89d7d9e82a5ba6f9c059f5302e17d3888576448f
Lenix <lenix01x@gmail.com>
2025-10-27 23:41:30 +0100

    progress saved

--- modules/require/shared.lua
+++ modules/require/shared.lua
@@ -1,27 +1,41 @@
 local loaded = {}
 
-package = {
+local package = {
     loaded = setmetatable({}, {
         __index = loaded,
     })
 }
 
-local function require(modulePath)
-    local resourceName = GetInvokingResource() or GetCurrentResourceName()
-    
+function require(modulePath)
+    print('called')
     assert(modulePath, 'Module path caught nil')
+
+    local resourceName, actualPath
     
-    local cacheKey = resourceName .. ':' .. modulePath
+    if modulePath:sub(1, 1) == '@' then
+        local separatorPos<const> = modulePath:find('/', 2)
+        if separatorPos then
+            resourceName = modulePath:sub(2, separatorPos - 1)
+            actualPath = modulePath:sub(separatorPos + 1)
+        else
+            error('Invalid module path format: ' .. modulePath)
+        end
+    else
+        resourceName = GetInvokingResource() or GetCurrentResourceName()
+        actualPath = modulePath
+    end
+    
+    local cacheKey<const> = resourceName .. ':' .. actualPath
     
     if package.loaded[cacheKey] then
         return package.loaded[cacheKey]
     end
 
-    local module = ('%s.lua'):format(modulePath)
-    local fileContent = LoadResourceFile(resourceName, module)
+    local module = ('%s.lua'):format(actualPath)
+    local fileContent<const> = LoadResourceFile(resourceName, module)
 
     if fileContent then
-        local chunk, err = load(fileContent, '@' .. resourceName .. '/' .. module)
+        local chunk<const>, err<const> = load(fileContent, '@' .. resourceName .. '/' .. module)
         if not chunk then
             error(err)
         end
@@ -30,6 +44,8 @@ local function require(modulePath)
         package.loaded[cacheKey] = module
         return module
     else
-        error('Module "' .. modulePath .. '" not found in resource "' .. resourceName .. '"')
+        error('Module "' .. actualPath .. '" not found in resource "' .. resourceName .. '"')
     end
 end exports('require', require)
+
+return require
