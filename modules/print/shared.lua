928ffd2cb57d3ce159c679361e33e8e5e17aff7e
Lenix <lenix01x@gmail.com>
2025-10-27 23:38:36 +0100

    update: merge the backward compatibility module

--- modules/print/shared.lua
+++ modules/print/shared.lua
@@ -1,6 +1,21 @@
-local types<const> = exports.tr_lib:load 'config'
+local types<const> = lib.load 'config'
 
-function CreatePrint(logType, message, path, line)
+local function backwardCompatPrint(data)
+  if type(data) ~= "table" then
+    print(data)
+    warn(("exports.tr_lib:print expected a table and received %s"):format(type(data):upper()))
+    return
+  end
+  assert(data.type ~= nil, ('invalid print type, received %s'):format(type(data.type):upper()))
+  assert(data.message ~= nil, ('invalid print message, received %s'):format(type(data.message):upper()))
+  
+  local logType<const> = data.type
+  local message<const> = data.message
+  
+  createPrint(logType, message, data.path, data.line)
+end
+
+local function createPrint(logType, message, path, line)
   local colors<const> = {
     error = '^1',
     err = '^1',
@@ -41,42 +56,71 @@ function CreatePrint(logType, message, path, line)
   end
 end
 
-local printFunc<const> = {
+local print<const> = {
   error = function(message, path, line)
-    CreatePrint('error', message, path, line)
+    if type(message) == "table" then
+      backwardCompatPrint(data)
+      return
+    end
+    createPrint('error', message, path, line)
   end,
   err = function(message, path, line)
-    CreatePrint('err', message, path, line)
+    if type(message) == "table" then
+      backwardCompatPrint(data)
+      return
+    end
+    createPrint('err', message, path, line)
   end,
   warning = function(message, path, line)
-    CreatePrint('warning', message, path, line)
+    if type(message) == "table" then
+      backwardCompatPrint(data)
+      return
+    end
+    createPrint('warning', message, path, line)
   end,
   warn = function(message, path, line)
-    CreatePrint('warn', message, path, line)
+    if type(message) == "table" then
+      backwardCompatPrint(data)
+      return
+    end
+    createPrint('warn', message, path, line)
   end,
   info = function(message, path, line)
-    CreatePrint('info', message, path, line)
+    if type(message) == "table" then
+      backwardCompatPrint(data)
+      return
+    end
+    createPrint('info', message, path, line)
   end,
   inf = function(message, path, line)
-    CreatePrint('inf', message, path, line)
+    if type(message) == "table" then
+      backwardCompatPrint(data)
+      return
+    end
+    createPrint('inf', message, path, line)
   end,
   success = function(message, path, line)
-    CreatePrint('success', message, path, line)
+    if type(message) == "table" then
+      backwardCompatPrint(data)
+      return
+    end
+    createPrint('success', message, path, line)
   end,
   log = function(message, path, line)
-    CreatePrint('log', message, path, line)
+    if type(message) == "table" then
+      backwardCompatPrint(data)
+      return
+    end
+    createPrint('log', message, path, line)
   end,
   debug = function(message, path, line)
-    CreatePrint('debug', message, path, line)
+    if type(message) == "table" then
+      backwardCompatPrint(data)
+      return
+    end
+    createPrint('debug', message, path, line)
   end,
-  legacy = BackwardCompatPrint --[[ for psychopaths XD ]]
+  legacy = backwardCompatPrint --[[ for psychopaths XD ]]
 }
 
-exports('print', function(data, path, line)
-  if type(data) == "table" then
-    BackwardCompatPrint(data)
-    return
-  end
-  
-  return printFunc
-end)
+return print
