491ffce76e115f0c4c20badb3d89e35cfed534d2
Lenix <lenix01x@gmail.com>
2025-10-30 16:18:59 +0100

    update: added tr_lib as a dependency

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,9 @@
 fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
+dependencies {
+    'tr_lib',
+}
 
 author 'Trippler'
 docs 'https://docs.trippler.store'
@@ -8,5 +11,4 @@ version '1.0.1'
 
 client_script 'client/build/main.js'
 ui_page 'web/index.html'
-
 file 'web/**'
