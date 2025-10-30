d3d611d455ac3071c4fea926fa5abab67e4107b5
Lenix <lenix01x@gmail.com>
2025-10-30 16:18:00 +0100

    improve: executed the todo task, added configuration

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,17 +1,17 @@
 fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
+dependencies {
+  'tr_lib'
+}
 
 author 'Trippler'
 docs 'https://docs.trippler.store'
-version '1.0.0'
+version '1.0.1'
 
 client_scripts {
   'client/bridge.js',
   'client/build/main.js',
 }
 ui_page 'web/index.html'
-files {
-  'web/build/main.js',
-  'web/index.html',
-}
+file 'web/**'
