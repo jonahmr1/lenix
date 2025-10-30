857e37b20a2a564aafd9630d759a5cd124d19085
Lenix <lenix01x@gmail.com>
2025-10-30 16:23:57 +0100

    update: dependencies, removed the utilities

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,18 +2,17 @@ fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
 dependencies {
+  'tr_lib',
   'pma-voice',
   'qb-core',
   'lenix_fusion',
-  'tr_lib'
 }
 
 author 'Trippler'
 docs 'https://docs.trippler.store'
-version '1.0.0'
+version '1.0.1'
 
 client_scripts {
-  'client/utils.lua',
   'client/main.lua',
 }
 server_script 'server/main.lua'
