c32b3a4fe9f03e1d2ff62db4409192081ea60e58
Lenix <lenix01x@gmail.com>
2025-11-01 13:02:30 +0100

    update: script dependency

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,17 +2,16 @@ fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
 dependencies {
-  'tr_lib'
+  'tr_lib',
+  'qb-core'
 }
 
 author 'Trippler'
 docs 'https://docs.trippler.store'
 version '1.0.2'
 
-files {
-  'config.json',
-}
 client_scripts {
+  'config.js',
   'client/bridge.js',
   'client/main.js',
 }
