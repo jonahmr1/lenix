f89929b5c53fd79800afbe838468ff3beb51cdfb
Lenix <lenix01x@gmail.com>
2025-11-10 00:00:23 +0100

    refactor: remove unused tableFiller and deepMerge functions from main.js

--- client/main.js
+++ client/main.js
@@ -5,45 +5,6 @@ function returnVehicle(key) {
     
 }
 
-function tableFiller(...objects) {
-    objects = objects.filter(obj => obj != null);
-    
-    if (objects.length === 0) return {};
-    if (objects.length === 1) return { ...objects[0] };
-    
-    const result = {};
-    
-    for (const obj of objects) {
-        deepMerge(result, obj);
-    }
-    
-    return result;
-}
-
-function deepMerge(target, source) {
-    for (const key in source) {
-        if (source.hasOwnProperty(key)) {
-            const sourceValue = source[key];
-            const targetValue = target[key];
-            
-            if (
-                sourceValue && 
-                typeof sourceValue === 'object' && 
-                !Array.isArray(sourceValue) &&
-                targetValue &&
-                typeof targetValue === 'object' &&
-                !Array.isArray(targetValue)
-            ) {
-                target[key] = deepMerge({ ...targetValue }, sourceValue);
-            } else {
-                target[key] = sourceValue;
-            }
-        }
-    }
-    
-    return target;
-}
-
 onNet('QBCore:Client:OnPlayerLoaded', function() {
     init()
 })
