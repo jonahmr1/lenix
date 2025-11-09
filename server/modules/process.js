4dc1f20c115db9aff1ad34cf0760bfac9b463582
Lenix <lenix01x@gmail.com>
2025-11-10 00:02:06 +0100

    feat: add tableFiller and deepMerge functions for object merging

--- server/modules/process.js
+++ server/modules/process.js
@@ -1,36 +1,55 @@
 onNet('lenix_vehicles:proccess', async (systemKey, configIndex) => {
     const src = source
     const Player = QBCore.Functions.GetPlayer(src)
-    const isRegisterable = typeof Items[systemKey][configIndex].registerable == 'boolean' ? Items[systemKey][configIndex].registerable : Items['_DEFAULT'].registerable
+    const selectedConfig = System[systemKey].ITEM
+    const proccessedItems = tableFiller(Items._DEFAULT, Items[selectedConfig][configIndex])
+    const isRegisterable = proccessedItems.registerable
+    
     if (isRegisterable) {
-        if (Player.PlayerData.money.cash >= (Items[systemKey][configIndex].price || Items['_DEFAULT'].price)) {
-            const handle = await spawnBoughtVehicle(true, systemKey, configIndex)
-            if (handle) {
-                Player.Functions.RemoveMoney("cash", Items[systemKey][configIndex].price || Items['_DEFAULT'].price)
+        if (Player.PlayerData.money.cash >= (proccessedItems.price)) {
+            const netId = await spawnBoughtVehicle(true, systemKey, configIndex, src)
+            if (netId) {
+                Player.Functions.RemoveMoney("cash", proccessedItems.price)
                 emitNet('QBCore:Notify', src, 'Vehicle Successfully Bought', "success")
             } else {
-                emitNet('QBCore:Notify', src, 'Vehicle Failed To Spawn !', "error")
+                lib.print.err('failed to sell the car to the player with the id of: ' + src)
             }
         } else {
             emitNet('QBCore:Notify', src, 'You Don\'t Have Enough Money !', "error")
         }
     } else {
-        const handle = await spawnBoughtVehicle(false, systemKey, configIndex)
-        if (handle) {
+        const netId = await spawnBoughtVehicle(false, systemKey, configIndex, src)
+        if (netId) {
             emitNet('QBCore:Notify', src, 'Vehicle Successfully took out', "success")
         } else {
-            emitNet('QBCore:Notify', src, 'Vehicle Failed To Spawn !', "error")
+            lib.print.err('failed to sell the car to the player with the id of: ' + src)
         }
     }
 })
 
-async function spawnBoughtVehicle(isRegisterable, systemKey, configIndex) {
-    const handle = await exports.tr_kit.createSingleVehicle({
-        hash: Items[systemKey][configIndex].model,
-        coords: systemKey.VEHICLES.spawn,
+async function spawnBoughtVehicle(isRegisterable, systemKey, configIndex, src) {
+    const selectedConfig = System[systemKey].ITEM
+    const proccessedItems = tableFiller(Items._DEFAULT, Items[selectedConfig][configIndex])
+    const netId = await exports.tr_kit.createSingleVehicle({
+        hash: GetHashKey(Items[selectedConfig][configIndex].vehicle),
+        coords: System[systemKey].VEHICLES.spawn,
     })
-    if (isRegisterable) {
-        emit('lenix_patrolvehicles:insert', [], Items[systemKey][configIndex].model, GetHashKey(Items[systemKey][configIndex].model), Items[systemKey][configIndex].plate)
+    if (netId) {
+        if (isRegisterable) {
+            const response = await insertSQL(
+                [],
+                {
+                    model: proccessedItems.vehicle,
+                    hash: GetHashKey(proccessedItems.vehicle)
+                },
+                proccessedItems.plate[0],
+                src
+            )
+            if (response) {
+                return netId
+            }
+        }
+        return netId
     }
-    return handle
+    return false
 }
