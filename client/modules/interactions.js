a8607fa16e7af8e141da6ed61374a1f11757ea0f
Lenix <lenix01x@gmail.com>
2025-11-10 18:19:38 +0100

    feat: add return vehicle interaction to ped elements and remove unused interface module

--- client/modules/interactions.js
+++ client/modules/interactions.js
@@ -1,41 +1,55 @@
+
 function createInteractions() {
-    for (const [key, value] of Object.entries(System)) {
-        if (key === "_DEFAULT") continue
-        for (const pedElement of value.PEDS.peds) {
-            const zoneName = `${GetCurrentResourceName()}_${key}_${pedElement}`
-            const interactions = tableFiller(System._DEFAULT.INTERACTIONS, pedElement.INTERACTIONS)
-            exports['qb-target'].AddBoxZone(
-                zoneName,
-                {
-                    x: pedElement.coords[0],
-                    y: pedElement.coords[1],
-                    z: pedElement.coords[2] + 1.0
-                },
-                3.45,
-                3.35,
-                {
-                    name: zoneName,
-                    heading: pedElement.coords[3],
-                    debugPoly: interactions.debug,
-                    minZ: pedElement.coords[2] - 1.0,
-                    maxZ: pedElement.coords[2] + 1.0,
-                }, 
-                {
-                    options: [
-                        {
-                            type: "client",
-                            icon: interactions.take.icon,
-                            label: interactions.take.label,
-                            job: interactions.targets.job,
-                            gang: interactions.targets.gang,
-                            action: function() {
-                                Menu.main(key)
-                            },
-                        },
-                    ],
-                    distance: interactions.take.distance,
-                }
-            )
+  for (const [key, value] of Object.entries(System)) {
+    if (key === "_DEFAULT") continue
+    for (const pedElement of value.PEDS.peds) {
+      const zoneName = `${GetCurrentResourceName()}_${key}_${pedElement}`
+      const interactions = tableFiller(System._DEFAULT.INTERACTIONS, pedElement.INTERACTIONS)
+      exports['qb-target'].AddBoxZone(
+        zoneName,
+        {
+          x: pedElement.coords[0],
+          y: pedElement.coords[1],
+          z: pedElement.coords[2] + 1.0
+        },
+        3.45,
+        3.35,
+        {
+          name: zoneName,
+          heading: pedElement.coords[3],
+          debugPoly: interactions.debug,
+          minZ: pedElement.coords[2] - 1.0,
+          maxZ: pedElement.coords[2] + 1.0,
+        },
+        {
+          options: [
+            {
+              type: "client",
+              icon: interactions.take.icon,
+              label: interactions.take.label,
+              job: interactions.targets.job,
+              gang: interactions.targets.gang,
+              action: function () {
+                Menu.main(key)
+              },
+            },
+            {
+              type: "client",
+              icon: interactions.return.icon,
+              label: interactions.return.label,
+              job: interactions.targets.job,
+              gang: interactions.targets.gang,
+              canInteract: function () {
+                return netIdsRequested.length !== 0
+              },
+              action: async function () {
+                await returnVehicle(netIdsRequested)
+              },
+            },
+          ],
+          distance: interactions.take.distance,
         }
+      )
     }
+  }
 }
