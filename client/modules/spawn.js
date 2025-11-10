9d719c575d15b3feb4f4cbf8fd4deba3e69cf317
Lenix <lenix01x@gmail.com>
2025-11-10 13:25:37 +0100

    refactor: simplify vehicle spawning logic and replace callback with new prepareVehicle function

--- client/modules/spawn.js
+++ client/modules/spawn.js
@@ -1,48 +1,15 @@
-let car
-function GetVehicleConfigByModel(model) {
-    for (let i = 0; i < Config.Vehicles.length; i++) {
-        for (let j = 0; j < Config.Vehicles[i].length; j++) {
-            if (Config.Vehicles[i][j].vehicle == model) {
-                return Config.Vehicles[i][j]
-            }
-        }
+lib.callback.register('prepareVehicle', function(handle, processedStyle) {
+    exports['qb-fuel'].SetFuel(handle, 100)
+    SetVehicleEngineOn(handle, false, false, false)
+
+    if (processedStyle && !processedStyle.isDisabled) {
+        SetVehicleCustomPrimaryColour(handle, processedStyle[0], processedStyle[1], processedStyle[2])
+        SetVehicleCustomSecondaryColour(handle, processedStyle[0], processedStyle[1], processedStyle[2])
+        SetVehicleLivery(handle, processedStyle.livery)
+        SetVehicleMod(handle, 48, processedStyle.livery, false)
     }
-    return null
-}
-
-onNet("lenix_patrolvehicles:spawn", function(vehicleModel, spawnCoords) {
-    let vehicleConfig = GetVehicleConfigByModel(vehicleModel)
-    if (vehicleConfig) {
-        QBCore.Functions.SpawnVehicle(vehicleModel, function(veh) {
-            let props = QBCore.Functions.GetVehicleProperties(veh)
-            let plate = vehicleConfig.plate[1] + math.random(vehicleConfig.plate[2], vehicleConfig.plate[3])
-            SetVehicleNumberPlateText(veh, plate)
-            exports[Config.FuelSystem].SetFuel(veh, 100)
-
-            if (vehicleConfig.style && vehicleConfig.style.isenabled) {
-                if (vehicleConfig.style.r && vehicleConfig.style.g && vehicleConfig.style.b) {
-                    SetVehicleCustomPrimaryColour(veh, vehicleConfig.style.r, vehicleConfig.style.g, vehicleConfig.style.b)
-                }
-                if (vehicleConfig.style.r && vehicleConfig.style.g && vehicleConfig.style.b) {
-                    SetVehicleCustomSecondaryColour(veh, vehicleConfig.style.r, vehicleConfig.style.g, vehicleConfig.style.b)
-                }
-                if (vehicleConfig.style.livery) {
-                    SetVehicleLivery(veh, vehicleConfig.style.livery)
-                    SetVehicleMod(veh, 48, vehicleConfig.style.livery, false)
-                }
-            }
-
-            if (vehicleConfig.Registerable) {
-                TriggerServerEvent("lenix_patrolvehicles:insert", props, vehicleModel, GetHashKey(vehicleModel), QBCore.Functions.GetPlate(veh))
-            }
-
-            TriggerEvent("vehiclekeys:client:SetOwner", plate)
-            SetVehicleEngineOn(veh, false, false, false)
-            car = veh
-        }, spawnCoords, true)
-    } 
+    return true
 })
-
 onNet('lenix_patrolvehicles:return', function() {
     if (car !== null) {
         QBCore.Functions.Notify('Vehicle Returned Back!')
