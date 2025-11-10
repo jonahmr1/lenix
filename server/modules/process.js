30cf3c17005a4c87ee1d4f5794c1c7bd2548b368
Lenix <lenix01x@gmail.com>
2025-11-10 22:51:20 +0100

    fix: pass processed items to addReturnOption event in spawnBoughtVehicle function

--- server/modules/process.js
+++ server/modules/process.js
@@ -4,7 +4,7 @@ onNet('lenix_vehicles:proccess', async (systemKey, configIndex) => {
     const selectedConfig = System[systemKey].ITEM
     const proccessedItems = tableFiller(Items._DEFAULT, Items[selectedConfig][configIndex])
     const isRegisterable = proccessedItems.registerable
-    
+
     if (isRegisterable) {
         if (Player.PlayerData.money.cash >= (proccessedItems.price)) {
             const netId = await spawnBoughtVehicle(true, systemKey, configIndex, src)
@@ -60,7 +60,7 @@ async function spawnBoughtVehicle(isRegisterable, systemKey, configIndex, src) {
                 return netId
             }
         } else {
-            emitNet('lenix_vehicle:client:addReturnOption', src, netId)
+            emitNet('lenix_vehicle:client:addReturnOption', src, netId, proccessedItems)
         }
         return netId
     } else {
