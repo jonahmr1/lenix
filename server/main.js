05b5c4af1fe87913c38245133eee52f00280969a
Lenix <lenix01x@gmail.com>
2025-11-10 13:26:23 +0100

    feat: add generatePlate function to create random license plates

--- server/main.js
+++ server/main.js
@@ -1,2 +1,8 @@
-Config = {}
 let isActive = false
+
+function generatePlate(plateArray) {
+  const plateString = plateArray[0]
+  const plateMinimumInteger = plateArray[1]
+  const plateMaximumInteger = plateArray[2]
+  return `${plateString}${Math.floor(Math.random() * (plateMaximumInteger - plateMinimumInteger + 1)) + plateMinimumInteger}`
+}
