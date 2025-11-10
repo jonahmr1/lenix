db347a640296a1d5dae60a53bdb7d9b574a87074
Lenix <lenix01x@gmail.com>
2025-11-10 22:54:56 +0100

    refactor: use const for resource and value variables in metadata function

--- modules/metadata/shared.lua
+++ modules/metadata/shared.lua
@@ -1,23 +1,23 @@
 function lib.metadata(keys, resourceName)
-  local resource = resourceName or GetInvokingResource() or GetCurrentResourceName()
+  local resource<const> = resourceName or GetInvokingResource()
 
   if not keys then
     return nil
   end
-  
+
   local metadata = {}
-  
+
   if type(keys) == 'string' then
-    local value = GetResourceMetadata(resource, keys, 0)
+    local value<const> = GetResourceMetadata(resource, keys, 0)
     return value
   end
-  
+
   for _, key in ipairs(keys) do
-    local value = GetResourceMetadata(resource, key, 0)
+    local value<const> = GetResourceMetadata(resource, key, 0)
     if value then
       metadata[key] = value
     end
   end
-  
+
   return metadata
 end
