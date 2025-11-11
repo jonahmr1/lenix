85ca9327ea2fabbb900abf5ea6b5722e06099f39
Lenix <lenix01x@gmail.com>
2025-11-11 23:54:41 +0100

    feat: add function to get the closest vehicle to a player

---
+++ modules/get/closestVehicle/server.lua
@@ -0,0 +1,24 @@
+function lib.getPlayerClosestVehicle(radialSpace, source)
+  assert(type(radialSpace) == 'number', ('radialSpace must be a number, received (%s)'):format(radialSpace))
+  assert(type(source) == 'number', ('source must be a number, received (%s)'):format(source))
+
+  local playerPed<const> = GetPlayerPed(source)
+  local coords<const> = GetEntityCoords(playerPed)
+  local vehicles<const> = GetGamePool('CVehicle')
+
+  local closestVehicle = nil
+  local closestDistance = radialSpace
+
+  for i = 1, #vehicles do
+    local vehicle<const> = vehicles[i]
+    local vehCoords<const> = GetEntityCoords(vehicle)
+    local distance<const> = #(coords - vehCoords)
+
+    if distance < closestDistance then
+      closestDistance = distance
+      closestVehicle = vehicle
+    end
+  end
+
+  return closestVehicle
+end
