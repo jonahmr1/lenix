3f76664476694ac5c907c8463c5279adbe2a1cbe
Lenix <lenix01x@gmail.com>
2025-11-11 10:21:28 +0100

    fix(db): update player data retrieval method in insertSQL function

--- server/modules/db.js
+++ server/modules/db.js
@@ -1,9 +1,9 @@
 async function insertSQL(mods, vehicle, plate, src) {
     const source = src
-    let Player = await QBCore.Functions.GetPlayer(source)
+    let playerData = await Bridge.getPlayerData(source)
     const response = await exports.oxmysql.insert_async('INSERT INTO player_vehicles (license, citizenid, vehicle, hash, mods, plate, state) VALUES (?, ?, ?, ?, ?, ?, ?)', [
-        Player.PlayerData.license,
-        Player.PlayerData.citizenid,
+        playerData.license,
+        playerData.citizenid,
         vehicle.model,
         vehicle.hash,
         JSON.stringify(mods),
