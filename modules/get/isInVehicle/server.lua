a742159f5b2313a1ce1c9440b4c1c54a0b0e480c
Lenix <lenix01x@gmail.com>
2025-11-12 00:00:13 +0100

    feat(get): add isInVehicle function to check if a player is in a vehicle

---
+++ modules/get/isInVehicle/server.lua
@@ -0,0 +1,6 @@
+function lib.isInVehicle(source)
+  assert(type(source) == 'number', ('source must be a number, received (%s)'):format(source))
+
+  local player<const> = GetPlayerPed(source)
+  return GetVehiclePedIsIn(player, false)
+end
