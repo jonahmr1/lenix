cbfc7e664b9bc7db3a23f75fe66bc5b154c8b6f3
Lenix <lenix01x@gmail.com>
2025-11-12 00:06:37 +0100

    feat: add type assertions for parameters in split function

--- modules/split/shared.lua
+++ modules/split/shared.lua
@@ -1,4 +1,7 @@
 function lib.split(str, delimiter)
+  assert(type(str) == 'string', ('str must be a string, got %s'):format(type(str)))
+  assert(type(delimiter) == 'string', ('delimiter must be a string, got %s'):format(type(delimiter)))
+
   delimiter = delimiter or ','
   local result = {}
   for match in string.gmatch(str, "([^" .. delimiter .. "]+)") do
