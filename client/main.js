e681276026d35304f5c1b9d738d5ea395d371f49
Lenix <lenix01x@gmail.com>
2025-11-11 10:19:38 +0100

    refactor: simplify player job initialization and remove unused variables

--- client/main.js
+++ client/main.js
@@ -1,17 +1,9 @@
+let NetIdsRequested = []
 let PlayerJob = {}
-let netIdsRequested = []
-const Vehicles = exports['qb-core'].GetSharedVehicles()
-
-onNet('QBCore:Client:OnPlayerLoaded', function() {
-    init()
-})
-
-onNet('QBCore:Client:OnJobUpdate', function(UpdatedData) {
-    PlayerJob = UpdatedData
-})
-function init() {
-    PlayerJob = QBCore.Functions.GetPlayerData().job
+function Initialization(currentJob) {
+    PlayerJob = currentJob
     createPeds()
     createInteractions()
 }
-init() // for dev mode only
+
+Initialization(Bridge.getPlayerJob()) // for dev mode only
