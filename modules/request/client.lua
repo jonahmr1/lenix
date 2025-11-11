84293f4ce02aad63273963a30ed297d9296f2eaf
Lenix <lenix01x@gmail.com>
2025-11-12 00:02:37 +0100

    feat(request): add type assertions for model and timeout parameters

--- modules/request/client.lua
+++ modules/request/client.lua
@@ -1,4 +1,7 @@
 function lib.requestModel(model, timeout)
+  assert(type(model) == 'string', ('model must be a string of hash, got %s'):format(type(model)))
+  assert(timeout == nil or type(timeout) == 'number', ('timeout must be a number, got %s'):format(type(timeout)))
+
   RequestModel(model)
   timeout = timeout or 10000
   while not HasModelLoaded(model) do
