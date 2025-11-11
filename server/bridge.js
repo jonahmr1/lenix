72f1d3e0a17b47e2607a6cf4a06a920a08425c4c
Lenix <lenix01x@gmail.com>
2025-11-11 17:25:48 +0100

    fix: update menu preview to use default image and improved title formatting

--- server/bridge.js
+++ server/bridge.js
@@ -16,3 +16,15 @@ Bridge.notify = function(source, message, type) {
 Bridge.giveKeys = function(plate) {
   emitNet('qb-vehiclekeys:server:AcquireVehicleKeys', plate)
 }
+
+Bridge.getPlayerJob = function(source) {
+  const job = QBCore.Functions.GetPlayer(source).PlayerData.job
+  const gradeLevel = QBCore.Functions.GetPlayer(source).PlayerData.job.grade.level
+  return { name: job, grade: gradeLevel}
+}
+
+Bridge.getPlayerGang = function(source) {
+  const gang = QBCore.Functions.GetPlayer(source).PlayerData.gang
+  const gradeLevel = QBCore.Functions.GetPlayer(source).PlayerData.gang.grade.level
+  return { name: gang, grade: gradeLevel}
+}
