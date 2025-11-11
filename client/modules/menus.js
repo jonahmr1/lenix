93b28889069413182d545c88ad36601ddb8eee3f
Lenix <lenix01x@gmail.com>
2025-11-11 17:23:28 +0100

    feat: add player permission check for allowed and disallowed targets

--- client/modules/menus.js
+++ client/modules/menus.js
@@ -3,3 +3,17 @@ function IsZoneFree(zone) {
   const response = lib.isZoneFree({ coords: zone, radius: 3.5 })
   return response
 }
+
+async function isPlayerAllowed(processedItem) {
+  const allowedTargets = processedItem.allowed
+  const disallowedTargets = processedItem.disallowed
+  let isAllowed = false
+  
+  const inAllowList = await lib.callback.await('isPlayerInAllowList', null, allowedTargets)
+  const notInDisallowList = await lib.callback.await('isPlayerNotInDisallowedList', null, disallowedTargets)
+
+  if (notInDisallowList) {
+    isAllowed = inAllowList ? true : false
+  }
+  return isAllowed
+}
