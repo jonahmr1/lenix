644e858a38655c2c299f329fceea05273ad9aebb
Lenix <lenix01x@gmail.com>
2025-11-11 23:39:50 +0100

    refactor(pedestrians): improve error handling and logging in ped creation functions

--- modules/pedestrians/server.js
+++ modules/pedestrians/server.js
@@ -1,9 +1,6 @@
 async function requestLoadResponse(model, timeout) {
     try {
         const response = await lib.callback.await('requestPedModel', null, -1, model, timeout);
-        if (!response) {
-            console.error(`Failed to load model: ${model}`);
-        }
         return response;
     } catch (error) {
         console.error(`Error in requestLoadResponse for model ${model}: ${error.message}`);
@@ -21,11 +18,17 @@ async function createSinglePed(settings) {
     const isNotValidCoords = ((typeof coords !== 'object' && Object.keys(coords).length !== 4) || (!Array.isArray(coords) && coords.length !== 4)) ? true : false
 
     if (typeof model !== 'string') {
-        console.log(`wrong type on the first argument, expected string of hash received ${typeof model} or ${model} indeed`)
+        console.error(`Invalid argument: expected a string hash as the first argument, but received type "${typeof model}" with value "${model}".`)
         return;
     }
     if (isNotValidCoords) {
-        console.error(`wrong type or missing a value on the second argument, (type: ${typeof coords}, values: x:${coords?.x || coords[0]}, y:${coords[1]}, z:${coords[2]}, w:${coords[3]})`)
+        console.error(
+        `Invalid argument: expected a valid vector4 object as the second argument, but received type "${typeof coords}" with values: ` +
+        `x: ${coords?.x ?? coords?.[0] ?? 'undefined'}, ` +
+        `y: ${coords?.y ?? coords?.[1] ?? 'undefined'}, ` +
+        `z: ${coords?.z ?? coords?.[2] ?? 'undefined'}, ` +
+        `w: ${coords?.w ?? coords?.[3] ?? 'undefined'}`
+        );
         return;
     }
     if (Array.isArray(coords)) {
@@ -38,24 +41,32 @@ async function createSinglePed(settings) {
 	}
 
     const response = await requestLoadResponse(model, 1000)
-    if (response) {
-        const ped = CreatePed(null, model, coords.x, coords.y, coords.z, coords.w, !!isAccessPublic, !!isAccessPublic && !isControlPublic);
-        if (scenario) {
-            if (scenario.freeze) FreezeEntityPosition(ped, true);
-            if (scenario.oblivious) emitNet('tr_kit:setBlockingOfNonTemporaryEvents', -1, ped);
-        }
+    if (response) return false;
+    const handle = CreatePed(null, model, coords.x, coords.y, coords.z, coords.w, !!isAccessPublic, !!isAccessPublic && !isControlPublic);
+    
+    const netId = await new Promise((resolve) => {
+		const tick = setTick(() => {
+			if (DoesEntityExist(handle)) {
+				const netId = NetworkGetNetworkIdFromEntity(handle)
+				clearTick(tick)
+				resolve(netId)
+			}
+		})
+	})
 
-        on('onResourceStop', async (resourceName) => {
-            if (GetCurrentResourceName() == resourceName) {
-                console.log(`${resourceName} caught stopping, clearing ped ${ped}`)
-                clearCreatedPed(ped)
-                await new Promise(resolve => setTimeout(resolve, 0))
-                emitNet('tr_kit:setEntityAsNoLongerNeeded', -1, ped)
-            }
-        })
-        return ped;
+    if (scenario) {
+        if (scenario.freeze) FreezeEntityPosition(handle, true);
+        if (scenario.oblivious) emitNet('tr_kit:setBlockingOfNonTemporaryEvents', -1, ped);
     }
-    return false
+
+    on('onResourceStop', async (resourceName) => {
+        if (GetCurrentResourceName() == resourceName) {
+            console.log(`${resourceName} caught stopping, clearing ped ${handle}`)
+            clearCreatedPed(netId)
+            emitNet('tr_kit:setEntityAsNoLongerNeeded', -1, handle)
+        }
+    })
+    return netId;
 }
 
 function createMultiplePeds(peds, defaultSettings) {
@@ -64,23 +75,32 @@ function createMultiplePeds(peds, defaultSettings) {
         return [];
     }
 
-    const created = [];
+    const netIds = [];
     for (let i = 0; i < peds.length; i++) {
         const ped = peds[i];
 
-        const model = ped.model ?? defaultSettings?.model;
-        const coords = ped.coords;
-        const isAccessPublic = ped.isAccessPublic ?? defaultSettings?.isAccessPublic;
-        const isControlPublic = ped.isControlPublic ?? defaultSettings?.isControlPublic;
+        const processedSettings = {
+            ...defaultSettings,
+            ...ped,
+            scenario: {
+                ...defaultSettings?.scenario,
+                ...ped?.scenario
+            }
+        }
+        const model = processedSettings.model;
+        const coords = processedSettings.coords;
+        const isAccessPublic = processedSettings.isAccessPublic;
+        const isControlPublic = processedSettings.isControlPublic;
 
         const scenario = {
-            name: ped.scenario?.name ?? defaultSettings?.scenario?.name,
-            freeze: ped.scenario?.freeze ?? defaultSettings?.scenario?.freeze,
-            timeToLeave: ped.scenario?.timeToLeave ?? defaultSettings?.scenario?.timeToLeave,
-            playIntroClip: ped.scenario?.playIntroClip ?? defaultSettings?.scenario?.playIntroClip
+            name: processedSettings.scenario?.name,
+            freeze: processedSettings.scenario?.freeze,
+            oblivious: processedSettings.scenario?.oblivious,
+            timeToLeave: processedSettings.scenario?.timeToLeave,
+            playIntroClip: processedSettings.scenario?.playIntroClip
         };
 
-        const createdPed = createSinglePed({ 
+        const netId = createSinglePed({ 
             model, 
             coords, 
             scenario, 
@@ -88,42 +108,54 @@ function createMultiplePeds(peds, defaultSettings) {
             isControlPublic 
         });
         
-        if (createdPed) created.push(createdPed);
+        if (netId) netIds.push(netId);
     }
-    return created;
+    return netIds;
 }
-function clearCreatedPed(entity) {
-    if (typeof entity !== 'number') {
-        console.log(`received ${typeof entity} instead of a number, if you passed an array of number to delete multiple peds, please use clearCreatedPeds instead of clearCreatePeds`)
+function clearCreatedPed(netId) {
+    if (typeof netId !== 'number') {
+        console.warn(`Invalid argument, expected a number, received ${typeof netId}`)
+        return false;
     }
-    DeleteEntity(entity);
+	const handle = NetworkGetEntityFromNetworkId(netId)
+    if (!DoesEntityExist(handle)) {
+        console.warn(`The entity with network id of ${netId} does not exist`)
+        return false;
+    }
+    DeleteEntity(handle);
     return true
 }
 
 function clearCreatedPeds(entities) {
     if (!Array.isArray(entities)) {
-        console.log(`received ${typeof entities} instead of array, use clearCreatedPed for single ped`)
+        console.log(`Invalid argument, expected an array, received ${typeof entities}`)
+        return false
     }
 
     for (let i = 0; i < entities.length; i++) {
-        DeleteEntity(entities[i]);
+        const handle = NetworkGetEntityFromNetworkId(entities[i])
+        if (!DoesEntityExist(handle)) {
+            console.warn(`The entity with network id of ${entities[i]} does not exist`)
+            continue;
+        }
+        DeleteEntity(handle);
     }
     return true
 }
 
-lib.callback.register('createSinglePed', function(settings) {
+lib.callback.register('createSinglePed', function(_, settings) {
     return createSinglePed(settings)
 })
 
-lib.callback.register('createMultiplePeds', function(peds, defaultSettings) {
+lib.callback.register('createMultiplePeds', function(_, peds, defaultSettings) {
     return createMultiplePeds(peds, defaultSettings)
 })
 
-lib.callback.register('clearCreatedPed', function(entity) {
+lib.callback.register('clearCreatedPed', function(_, entity) {
     return clearCreatedPed(entity)
 })
 
-lib.callback.register('clearCreatedPeds', function(entities) {
+lib.callback.register('clearCreatedPeds', function(_, entities) {
     return clearCreatedPeds(entities)
 })
 
