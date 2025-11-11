8a085a9b2cf6fba4406c12d5e233009c6c39ee53
Lenix <lenix01x@gmail.com>
2025-11-11 23:56:54 +0100

    refactor: remove unused vehicle and zone utility functions

--- modules/get/isInVehicle/shared.lua
+++
@@ -1,4 +0,0 @@
-function lib.isInVehicle(source)
-  local player<const> = IsDuplicityVersion() and (source and GetPlayerPed(source) or -1) or PlayerPedId()
-  return GetVehiclePedIsIn(player, false)
-end
