9e7a0d8137ee05d5f8df3aa023f2ad7f0072c99a
Lenix <lenix01x@gmail.com>
2025-11-12 17:25:54 +0100

    ✨ feat(client): add zone clearance check
    - implement IsZoneFree function to check if a zone is clear
    - use lib.isZoneClear to determine if the zone is clear of players

--- client/main.js
+++ client/main.js
@@ -1,9 +1,13 @@
 let NetIdsRequested = []
 let PlayerJob = {}
 function Initialization(currentJob) {
-    PlayerJob = currentJob
-    createPeds()
-    createInteractions()
+  PlayerJob = currentJob
+  createPeds()
+  createInteractions()
 }
 
+function IsZoneFree(zone) {
+  const response = lib.isZoneClear({ coords: zone, radius: 2 })
+  return response
+}
 Initialization(Bridge.getPlayerJob()) // for dev mode only
