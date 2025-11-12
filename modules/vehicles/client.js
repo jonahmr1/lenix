a02c925f481e9b34ed59ad93b5b70dd9969c0ec0
Lenix <lenix01x@gmail.com>
2025-11-12 16:24:52 +0100

    ✨ feat(vehicle): add visual effect when vehicle is created
    - add preCreateVehicle event to set entity alpha
    - set entity alpha to 51, 102 for 2 seconds
    - reset entity alpha to default after 2 seconds

--- modules/vehicles/client.js
+++ modules/vehicles/client.js
@@ -1,21 +1,38 @@
+onNet('tr_kit:client:preCreateVehicle', (netId) => {
+	const entity = NetworkGetEntityFromNetworkId(netId);
+	
+	for (let i = 0; i < 10; i++) {
+		setTimeout(() => {
+			SetEntityAlpha(entity, 51, false);
+			setTimeout(() => {
+				SetEntityAlpha(entity, 102, false);
+			}, 100);
+		}, i * 200);
+	}
+	
+	setTimeout(() => {
+		ResetEntityAlpha(entity);
+	}, 2000);
+});
+
 function createSingleVehicle(settings) {
-    return lib.callback.await('createSingleVehicle', null, settings)
+	return lib.callback.await('createSingleVehicle', null, settings)
 }
 
 function createMultipleVehicles(settings) {
-    return lib.callback.await('createMultipleVehicles', null, settings)
+	return lib.callback.await('createMultipleVehicles', null, settings)
 }
 
 function clearCreatedVehicle(vehicle) {
-    return lib.callback.await('clearCreatedVehicle', null, vehicle)
+	return lib.callback.await('clearCreatedVehicle', null, vehicle)
 }
 
 function clearCreatedVehicles(vehicles) {
-    return lib.callback.await('clearCreatedVehicles', null, vehicles)
+	return lib.callback.await('clearCreatedVehicles', null, vehicles)
 }
 
-lib.callback.register('requestModel', function(modelHash, timeout) {
-    return lib.requestModel(modelHash, timeout)
+lib.callback.register('requestModel', function (modelHash, timeout) {
+	return lib.requestModel(modelHash, timeout)
 })
 
 exports('createSingleVehicle', createSingleVehicle)
