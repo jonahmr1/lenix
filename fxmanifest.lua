e964a307dde07dc7d0e53bf588a4be8f7af176a9
Lenix <lenix01x@gmail.com>
2025-11-12 14:05:04 +0100

    ci: split client and server modules

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -19,19 +19,21 @@ shared_scripts {
   'modules/resources/shared.lua',
   'modules/metadata/shared.lua',
   'modules/filter/shared.lua',
-  'modules/get/closestVehicle/shared.lua',
-  'modules/get/isInVehicle/shared.lua',
 }
 
 client_scripts {
   'modules/callback/client.lua',
   'modules/request/client.lua',
   'modules/get/isZoneClear/client.lua',
+  'modules/get/closestVehicle/client.lua',
+  'modules/get/isInVehicle/client.lua',
 }
 
 server_scripts {
   'modules/callback/server.lua',
   'modules/version/server.lua',
+  'modules/get/closestVehicle/server.lua',
+  'modules/get/isInVehicle/server.lua',
 }
 
 shared_script 'get.lua'
