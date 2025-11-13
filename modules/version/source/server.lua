8427607280c2449d4653d075374db910ce36ae76
Lenix <lenix01x@gmail.com>
2025-11-13 15:28:18 +0100

    ✨ feat(version): implement source version checking for resources
    - add function to check resource version against remote repository
    - use resource metadata to determine current version and repository
    - compare local version with remote version from fxmanifest.lua in github repository
    - log information about version status to console

--- modules/version/source/server.lua
+++ modules/version/source/server.lua
@@ -1,3 +1,4 @@
+local getResourceMetadata = lib.require('modules/version/server').getResourceMetadata
 local parseGithubRepo = lib.require('modules/version/server').parseGithubRepo
 local getRepositoryResponse = lib.require('modules/version/server').getRepositoryResponse
 
@@ -13,26 +14,25 @@ local function getSourceVersion(response, metadata, resourceName)
     ))
     return false, remoteVersion
   else
-    lib.console.info(('%s is up to date (%s)'):format(resourceName, metadata.version))
+    lib.console.info(('%s source is up to date (%s)'):format(resourceName, metadata.version))
     return true, metadata.version
   end
 end
 
-function lib.version.source(repository, version, resourceName)
-  assert(repository, ('No repository provided, got %s'):format(repository))
+function lib.version.source(repositoryURL, currentVersion, targetResourceName)
+  local resourceName<const> = targetResourceName or GetInvokingResource() or GetCurrentResourceName()
 
-  local metadata <const> = {
-    version = version,
-    repository = repository
-  }
+  local metadata <const> = getResourceMetadata(currentVersion, resourceName, repositoryURL)
 
+  assert(metadata.repository, ('Got %s from %s\'s fxmanifest, pass repository URL argument or update your fxmanifest'):format(metadata.repository, resourceName))
   local owner, repoName = parseGithubRepo(metadata.repository)
   assert((owner and repoName) ~= nil, ('Failed to parse the repository, got owner: %s, repoName: %s'):format(owner, repoName))
 
-  local success<const>, response<const> = getRepositoryResponse(('https://raw.githubusercontent.com/%s/%s/refs/heads/main/fxmanifest.lua'):format(owner, repoName))
+  local apiURL<const> = ('https://raw.githubusercontent.com/%s/%s/refs/heads/main/fxmanifest.lua'):format(owner, repoName)
+  local success<const>, response<const> = getRepositoryResponse(apiURL)
   if not success then
-    return
+    return success, response
   end
 
-  return getSourceVersion(response, metadata, resourceName or GetInvokingResource() or GetCurrentResourceName())
+  return getSourceVersion(response, metadata, resourceName)
 end
