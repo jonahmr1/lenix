5cb24d77833109cb49effd63a3ae4cd50e8d350d
Lenix <lenix01x@gmail.com>
2025-11-15 00:50:21 +0100

    ✨ feat(pedestrians): add createSinglePed and clearCreatedPed
    - Implement createSinglePed to create a single ped with given settings
    - Implement clearCreatedPed to clear a single ped with given netId
    - Export createMultiplePeds and clearCreatedPeds

--- modules/pedestrians/client.js
+++ modules/pedestrians/client.js
@@ -1,39 +1,66 @@
-onNet('tr_kit:client:playPedScenario', async(handle, netId, name, timeToLeave, playIntroClip) => {
-    SetNetworkIdExistsOnAllMachines(netId, true);
-    await new Promise(resolve => setImmediate(resolve));
-    console.log(NetworkGetEntityIsNetworked(handle))
-    const entity = await lib.awaitNetworkExisting(netId)
-    if (!entity) return;
-    TaskStartScenarioInPlace(entity, name, timeToLeave, playIntroClip);
-});
+async function createSinglePed(settings) {
+    const hash = settings.hash;
+    const scenario = settings.scenario;
+    let coords = settings.coords;
 
-onNet('tr_kit:client:setEntityAsNoLongerNeeded', async (netId) => {
-    const entity = await lib.awaitNetworkExisting(netId)
-    if (!entity) return;
-    SetEntityAsNoLongerNeeded(entity)
-})
+    const isNotValidCoords = ((typeof coords !== 'object' && Object.keys(coords).length !== 4) || (!Array.isArray(coords) && coords.length !== 4)) ? true : false
 
-onNet('tr_kit:client:setBlockingOfNonTemporaryEvents', async (netId) => {
-    const entity = await lib.awaitNetworkExisting(netId)
-    if (!entity) return;
-    SetBlockingOfNonTemporaryEvents(entity, true)
-})
+    if (typeof hash !== 'number') {
+        console.error(`Expecter a number of hash, got ${typeof hash}(${hash})`)
+        return;
+    }
+    if (isNotValidCoords) {
+        console.error(
+        `Invalid argument: expected a valid vector4 object as the second argument, but received type "${typeof coords}" with values: ` +
+        `x: ${coords?.x ?? coords?.[0] ?? 'undefined'}, ` +
+        `y: ${coords?.y ?? coords?.[1] ?? 'undefined'}, ` +
+        `z: ${coords?.z ?? coords?.[2] ?? 'undefined'}, ` +
+        `w: ${coords?.w ?? coords?.[3] ?? 'undefined'}`
+        );
+        return;
+    }
+    if (Array.isArray(coords)) {
+		coords = {
+			x: coords[0],
+			y: coords[1],
+			z: coords[2],
+			w: coords[3]
+		}
+	}
 
-lib.callback.register('requestPedModel', async function(model, timeout) {
-    const response = await lib.requestModel(GetHashKey(model), timeout)
-    return response;
-});
+    const response = await lib.requestModel(hash, 1000)
+    if (!response) return;
 
-function createSinglePed(settings) {
-    return lib.callback.await('createSinglePed', 1000, settings)
+    const entityHandle = CreatePed(null, hash, coords.x, coords.y, coords.z, coords.w, true, true);
+    let [_, netId] = await lib.awaitInstanceExisting(entityHandle)
+    if (!netId) return;
+
+    if (scenario) {
+        if (scenario.name) TaskStartScenarioInPlace(entityHandle, scenario.name, scenario?.timeToLeave, scenario?.playIntroClip);
+        if (scenario.freeze) FreezeEntityPosition(entityHandle, true);
+        if (scenario.oblivious) SetBlockingOfNonTemporaryEvents(entityHandle, true);
+    }
+
+    on('onResourceStop', async (resourceName) => {
+        if (GetCurrentResourceName() == resourceName) {
+            console.log(`${resourceName} caught stopping, clearing ped (netId: ${netId})`)
+            SetEntityAsNoLongerNeeded(entityHandle, true)
+            clearCreatedPed(netId)
+        }
+    })
+    return entityHandle, netId
 }
+
+lib.callback.register('createSinglePed', (settings) => {
+    return createSinglePed(settings)
+})
 
 function createMultiplePeds(peds, defaultSettings) {
     return lib.callback.await('createMultiplePeds', null, peds, defaultSettings)
 }
 
-function clearCreatedPed(entity) {
-    return lib.callback.await('clearCreatedPed', null, entity)
+function clearCreatedPed(netId) {
+    return lib.callback.await('clearCreatedPed', null, netId)
 }
 
 function clearCreatedPeds(entities) {
