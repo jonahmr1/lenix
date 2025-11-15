40a8bd8522cd6f48128f67fb5c8ae2047f6764b9
Lenix <lenix01x@gmail.com>
2025-11-15 19:56:31 +0100

    🐛 fix(request): ensure model loading with hash
    - fix: change `model` to `hash` to ensure the model loads using the correct identifier

--- modules/request/client.lua
+++ modules/request/client.lua
@@ -1,10 +1,10 @@
-function lib.requestModel(model, timeout)
-  assert(type(model) == 'number', ('model must be a number of hash, got %s'):format(type(model)))
+function lib.requestModel(hash, timeout)
+  assert(type(hash) == 'number', ('hash must be a number, got %s'):format(type(hash)))
   assert(timeout == nil or type(timeout) == 'number', ('timeout must be a number, got %s'):format(type(timeout)))
 
-  RequestModel(model)
+  RequestModel(hash)
   timeout = timeout or 10000
-  while not HasModelLoaded(model) do
+  while not HasModelLoaded(hash) do
     timeout -= 1
     if timeout <= 0 then
       return false
