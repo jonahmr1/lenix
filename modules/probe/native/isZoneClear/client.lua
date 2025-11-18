3dfbf5f48c8bf8144e22b4d9fe84db50c1019cc5
Lenix <lenix01x@gmail.com>
2025-11-18 10:59:09 +0100

    🐛 fix(probe): fix isZoneClear export
    - fix exclude paramater of isZoneClear export
    - update version to 1.29.3

--- modules/probe/native/isZoneClear/client.lua
+++ modules/probe/native/isZoneClear/client.lua
@@ -1,12 +1,12 @@
-function lib.isZoneClear(inputCoords, radius, includeSelf)
+function lib.isZoneClear(inputCoords, radius, exclude)
   assert(inputCoords == nil or type(inputCoords) == "table", ("inputCoords must be a table, got %s"):format(type(inputCoords)))
   assert(radius == nil or type(radius) == "number", ("radius must be a number, got %s"):format(type(radius)))
-  assert(includeSelf == nil or type(includeSelf) == "boolean", ("includeSelf must be a boolean, got %s"):format(type(includeSelf)))
+  assert(exclude == nil or type(exclude) == "table", ("exclude must be a table, got %s"):format(type(exclude)))
 
   local radial <const> = radius or 10.0
   local coords
   if inputCoords then
-    coords = #inputCoords > 0 and vec3(inputCoords[1], inputCoords[2], inputCoords[3]) or inputCoords
+    coords = #inputCoords > 0 and { x = inputCoords[1], y = inputCoords[2], z = inputCoords[3]} or inputCoords
   else
     coords = GetEntityCoords(lib.player)
   end
@@ -14,11 +14,10 @@ function lib.isZoneClear(inputCoords, radius, includeSelf)
   local pools <const> = { 'Vehicle', 'Object' }
 
   for i = 1, #pools do
-    local entity = lib.closestEntity(coords, radial, pools[i])
+    local entity = lib.closestEntity(coords, radial, pools[i], exclude)
     if entity then return false end
   end
 
-  local exclude = not includeSelf and lib.player or nil
   local closestPed = lib.closestEntity(coords, radial, 'Ped', exclude)
   if closestPed then
     return false
