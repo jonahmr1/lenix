f904be27617c692152ea70a379bdb68e19c47a58
Lenix <lenix01x@gmail.com>
2025-11-20 17:58:52 +0100

    ✨ feat(resource): add web files to manifest
    - include web files for resource loading

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,13 @@
 fx_version 'cerulean'
 game 'gta5'
 
-client_script 'client.lua'
+client_script 'client/main.js'
 
 ui_page 'web/index.html'
+
+files {
+  'web/index.html',
+  'web/main.js',
+  'web/*.mp4',
+  'web/*.mp3'
+}
