854d6e8a948f1c1d3177b045d8d887f881795551
tripplerscripts <tripplerscripts@github.com>
2025-11-20 13:15:40 -0800

    ✨ feat(client): initialize NUI when content is loaded
    - add contentLoaded NUI callback to initialize the NUI
    - add loseFocus NUI callback to release focus from NUI

--- client/main.js
+++ client/main.js
@@ -1,8 +1,15 @@
-const interval = setInterval(() => {
-  if (NetworkIsSessionStarted()) {
-    SendNuiMessage(JSON.stringify({
-      action: 'init'
-    }))
-    clearInterval(interval)
-  }
-}, 500)
+RegisterNuiCallback('contentLoaded', (_, cb) => {
+  const interval = setInterval(() => {
+    if (NetworkIsSessionStarted()) {
+      SetNuiFocus(true, false)
+      SendNuiMessage(JSON.stringify({
+        action: 'init'
+      }))
+      clearInterval(interval)
+    }
+  }, 500)
+})
+
+RegisterNuiCallback('loseFocus', () => {
+  SetNuiFocus(false, false)
+})
