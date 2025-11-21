4f8588cca17c6fdc2ba4f1221fa665ccb5d70c35
Lenix <lenix01x@gmail.com>
2025-11-21 21:15:09 +0100

    first commit

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Trippler'
+version '1.0.0'
+repository 'https://github.com/TripplerScripts/tr_survey'
+
+ui_page 'web/index.html'
+
+files {
+  'web/index.html',
+  'web/main.js',
+}
