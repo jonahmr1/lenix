923a2db447d378c50b222c8030fe0ae75f1d2ddc
Lenix <149199196+LenixDev@users.noreply.github.com>
2025-11-22 23:44:54 -0800

    update: adjusted the lib callback's syntax

--- server/main.lua
+++ server/main.lua
@@ -2,7 +2,7 @@ local lib = exports.tr_lib:require [[@tr_lib/get]]
 
 AddEventHandler('entityCreated', function(handle)
   local source<const> = NetworkGetEntityOwner(handle)
-  local vehicleClass = lib.callback.await('getVehicleClass', source, 1000)
+  local vehicleClass = lib.callback.await('getVehicleClass', nil, source, 1000)
 
   local entityType = GetEntityType(handle) ~= nil and GetEntityType(handle) or nil
   if entityType and entityType == 2 and vehicleClass and vehicleClass == 18 then
