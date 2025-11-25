88d519c8443e35a12a4adce1b2833190eb4f3f61
Lenix <lenix01x@gmail.com>
2025-11-25 12:19:27 +0100

    ✨ feat(build): restructure build and config files
    - update gitignore to include build files and directories
    - move config.js to build directory
    - update client scripts path in fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,13 +1,13 @@
 fx_version 'cerulean'
 game 'gta5'
-lua54 'yes
+lua54 'yes'
 
 author 'Trippler'
 docs 'https://docs.trippler.store'
 version '1.0.5'
 
 client_scripts {
-    'config.js',
+    'build/config.js',
     'client/build/main.js',
 }
 ui_page 'web/index.html'
