b392a3522cdea7396a0d2de516ed396f566985d9
lenixdev <149199196+LenixDev@users.noreply.github.com>
2025-12-27 06:55:31 -0800

    fix(console): add reset color code to console outputs (server side problem only)
    - Append reset color code '^0' to trace console messages
    - Append reset color code '^0' to info console messages
    - Ensure console outputs reset color after custom coloring for proper formatting

--- package/src/shared/console/index.ts
+++ package/src/shared/console/index.ts
@@ -1,9 +1,9 @@
 export const trace = (...parameters: any): void => {
-  console.trace(`^6`, ...parameters)
+  console.trace(`^6`, ...parameters, '^0')
 }
 
 export const info = (...parameters: any): void => {
-  console.info(`^5`, ...parameters)
+  console.info(`^5`, ...parameters, '^0')
 }
 
 export const fatal = (...parameters: any): void => {
