a953a232b919f5cb6abe0cd109f8d57bd0336095
lenixdev <149199196+LenixDev@users.noreply.github.com>
2025-12-28 13:27:12 -0800

    chore(version): bump library version to 2.4.1
    - Updated fxmanifest.lua version from 2.3.0 to 2.4.1
    - Updated package.json version from 2.2.13 to 2.4.1

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,7 +5,7 @@ lua54 'yes'
 author 'Trippler'
 docs 'https://docs.trippler.store'
 repository 'https://github.com/TripplerScripts/tr_lib'
-version '2.3.0'
+version '2.4.1'
 
 files {
   'imports/version/server.lua',
