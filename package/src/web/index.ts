5bcf6e7ed67191bcead6db170a24ec396fa59c22
lenixdev <149199196+LenixDev@users.noreply.github.com>
2025-12-29 04:57:34 -0800

    chore(package): prepare version 2.7.1 release and update build script
    - Bump package version from 2.6.1 to 2.7.1
    - Modify patch script to remove client, server, shared, and web folders before build
    - Export onNuiCallback from web index alongside triggerNuiCallback

--- package/src/web/index.ts
+++ package/src/web/index.ts
@@ -2,4 +2,5 @@ declare global {
   const GetParentResourceName: () => string;
 }
 
+export { default as onNuiCallback } from './triggerNuiCallback'
 export { default as triggerNuiCallback } from './triggerNuiCallback'
