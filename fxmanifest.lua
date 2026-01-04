762132669e6d4203cddd584b102fad56096d6591
Lenix <149199196+LenixDev@users.noreply.github.com>
2026-01-04 06:23:08 +0100

    chore(fxmanifest): update version to 2.9.7
    - Bump fxmanifest version from 2.9.6 to 2.9.7
    - No other changes detected in manifest file

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,7 +5,7 @@ lua54 'yes'
 author 'Trippler'
 docs 'https://docs.trippler.store'
 repository 'https://github.com/TripplerScripts/tr_lib'
-version '2.9.6'
+version '2.9.7'
 
 files {
   'imports/version/server.lua',
