76d0d2248eb14e33e2f361c1e83a518abfcc0721
Lenix <149199196+LenixDev@users.noreply.github.com>
2026-03-19 03:15:31 +0100

    feat: initialize Rust project with Cargo configuration and main function

---
+++ ai/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+/* 
+    🏆 Best overall / most intelligent - openai/gpt-oss-120b
+    ⚡ Best for speed (chatbots, real-time apps) - llama-3.1-8b-instant
+    🧠 Best for reasoning / hard problems - qwen/qwen3-32b
+    🌐 Best for long documents = moonshotai/kimi-k2-instruct-0905 
+    🔧 Best for tool use / function calling - llama-3.3-70b-versatile 
+    🔍 Best with built-in web search - groq-compound
+*/
