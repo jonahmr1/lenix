8e344fafa49c1d3a8408681d51b148af9907e750
Lenix <149199196+LenixDev@users.noreply.github.com>
2026-03-23 16:05:42 +0100

    feat(lint): update lint configuration and remove outdated ESLint preset.

--- modules/src/lint/index.ts
+++
@@ -1,6 +0,0 @@
-import preset from "./preset.json"
-/**
- * Load eslint presets rules.
- * @see https://raw.githubusercontent.com/LenixDev/lenix/refs/heads/main/lint/src/lint-preset.json
- */
-export const lint = { strict: preset.rules }
