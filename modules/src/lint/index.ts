eb7bc035e90a34a11fbd9dae52c0e6a63e8b8513
Lenix <149199196+LenixDev@users.noreply.github.com>
2026-03-23 15:50:27 +0100

    feat(module-lint): Update eslint preset rules and rebuild configurations

--- modules/src/lint/index.ts
+++ modules/src/lint/index.ts
@@ -1,6 +1,6 @@
 import preset from "./preset.json"
 /**
- * Load eslint preset rules.
+ * Load eslint presets rules.
  * @see https://raw.githubusercontent.com/LenixDev/lenix/refs/heads/main/lint/src/lint-preset.json
  */
 export const lint = { strict: preset.rules }
