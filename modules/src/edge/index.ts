96a318241120c12ac6a607a8e37a75fdaa0535d9
Lenix <149199196+LenixDev@users.noreply.github.com>
2026-03-23 18:58:32 +0100

    feat(utilities): Add Edge interface and edge module to utilities

---
+++ modules/src/edge/index.ts
@@ -0,0 +1,22 @@
+export type Events = "users-management"
+export interface CreateUser<Role> {
+  identifier: string
+  role: Role
+  password: string
+}
+export interface UserAccount<Role, Label> {
+  id: string
+  identifier: string
+  role: Role
+  roleLabel: Label
+}
+export interface DeleteUser {
+  identifier: string
+}
+
+/**
+ * @private
+ * DON NOT USE
+ * @description
+ * A proxy between client and server contexts that can provide a one source of truth instead of duplicating codes, lack of clearancy, race conditions, ...
+ */
