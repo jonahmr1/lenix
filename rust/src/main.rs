c9124b8fccefc04e7973a52a8218e5e1763a9115
Lenix <149199196+LenixDev@users.noreply.github.com>
2026-04-03 16:14:07 +0100

    refactor(main): extract Player struct and impl from main, rename player variable

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,23 +1,22 @@
+struct Player {
+    name: String,
+    score: i32,
+}
+
+impl Player {
+    fn new(name: String, score: i32) -> Player {
+        Player { name, score }
+    }
+
+    fn describe(&self) -> String {
+        format!("{} has scored: {}", self.name, self.score)
+    }
+}
+
 fn main() {
     let score: i32 = 10;
-
     let _status: &str = if score > 5 { "win" } else { "lose" };
 
-    struct Player {
-        name: String,
-        score: i32,
-    }
-
-    impl Player {
-        fn new(name: String, score: i32) -> Player {
-            Player { name, score }
-        }
-
-        fn describe(&self) -> String {
-            format!("{} has scored: {}", self.name, self.score)
-        }
-    }
-
-    let p: Player = Player::new(String::from("ali"), score);
-    println!("{}", p.describe())
+    let player: Player = Player::new(String::from("Lenix"), score);
+    println!("{}", player.describe())
 }
