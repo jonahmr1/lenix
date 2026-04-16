64d8dd991391c57fc361358b10f08975fe92ba35
Lenix <149199196+LenixDev@users.noreply.github.com>
2026-04-16 14:52:23 +0100

    chore(fxmanifest): bump version to 2.0.1 and add tr_kit dependency

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,15 +2,16 @@ fx_version 'cerulean'
 game 'gta5'
 dependencies {
   'qbx_core',
-	'tr_lib'
+	'tr_lib',
+	'tr_kit'
 }
 
 author 'https://github.com/LenixDev'
-version '2.0.0'
+version '2.0.1'
 
+shared_script '@tr_lib/index.lua'
 client_script 'client/index.lua'
 server_script 'server/index.lua'
-shared_script '@tr_lib/index.lua'
 
 files {
 	'client/**/*.lua',
