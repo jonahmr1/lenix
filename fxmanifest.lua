721ab2b29183772898acbba2e44db9999e711eb6
Lenix <149199196+LenixDev@users.noreply.github.com>
2026-04-16 15:38:28 +0100

    build(fxmanifest): add dependencies, shared script, and reorganize client/server scripts

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,17 +1,34 @@
 fx_version 'cerulean'
 game 'gta5'
-dependencies {
-  'qbx_core',
-}
 
 author 'https://github.com/LenixDev'
 version '2.0.0'
 
-client_script 'build/client.js'
-server_script 'build/server.js'
+dependencies {
+  'qbx_core',
+  'pma-voice',
+  'tr_lib'
+}
+
+shared_script '@tr_lib/index.lua'
+
+client_scripts {
+  'client/index.lua',
+  'client/api/*.lua',
+  'client/modules/*.lua',
+  'client/nui/*.lua'
+}
+
+server_scripts {
+  'server/index.lua',
+  'server/api/*.lua',
+  'server/services/*.lua'
+}
 
 ui_page 'nui/public/index.html'
+
 files {
   'nui/public/index.html',
-  'build/nui.js'
+  'build/nui.js',
+  'shared/**/*.lua'
 }
