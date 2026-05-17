044dbb78f58137e6f064140bf9ac6f70e9f2c6ca
Lenix <149199196+LenixDev@users.noreply.github.com>
2026-05-17 14:52:35 +0100

    feat(index.php): display database columns in HTML table

--- php/index.php
+++ php/index.php
@@ -149,9 +149,19 @@
 						class='center'
 					>";
 						if ($columns && mysqli_num_rows($columns) > 0) {
+							echo "<table border='1' cellpadding='4' style='border-collapse:collapse'>";
+							echo "<tr><th>Column</th><th>Type</th><th>Null</th><th>Key</th><th>Default</th><th>Extra</th></tr>";
 							while ($col = mysqli_fetch_array($columns)) {
-								echo "<div>" . $col["Field"] . " — " . $col["Type"] . "</div>";
+									echo "<tr>
+											<td>{$col['Field']}</td>
+											<td>{$col['Type']}</td>
+											<td>{$col['Null']}</td>
+											<td>{$col['Key']}</td>
+											<td>{$col['Default']}</td>
+											<td>{$col['Extra']}</td>
+									</tr>";
 							}
+							echo "</table>";
 						}
 					echo "</div>";
 				echo "</div>";
