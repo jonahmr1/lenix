ae21f0871e3e68830c3d2b05b46b71f2e0d6c617
Lenix <149199196+LenixDev@users.noreply.github.com>
2026-05-20 17:18:38 +0100

    docs(types): add JSDoc comment for S type

--- modules/types/index.ts
+++ modules/types/index.ts
@@ -1,3 +1,9 @@
 import type { Dispatch, SetStateAction } from 'npm:@types/react@^19'
 
+/**
+ * The `useState` setter hook type.
+ * @template T - The type of the state.
+ * @example
+ * S<number | string>
+ */
 export type S<T> = Dispatch<SetStateAction<T>>
