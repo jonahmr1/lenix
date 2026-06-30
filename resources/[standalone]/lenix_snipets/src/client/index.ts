eaab648963f3d8b49bccc0ef712d691d5059a9f4
Lenix <149199196+lehnihx@users.noreply.github.com>
2026-07-01 02:57:28 +0300

    #

--- resources/[standalone]/lenix_snipets/src/client/index.ts
+++ resources/[standalone]/lenix_snipets/src/client/index.ts
@@ -1,13 +1,49 @@
-import { addRadialItem, notify } from "@overextended/ox_lib/client"
+//TODO: serverify the logic
+import { addRadialItem, cache, createVehicle, notify, requestModel } from "@overextended/ox_lib/client"
 
 interface Ped {
 	model: string
 	coords: [number, number, number, number]
+	scenario?: string
 }
 
-const PEDS: Ped[] = []
+const SPAWN_LOCATION: [number, number, number, number] = [-256.1688, -333.2883, 29.1064, 9.2070]
 
-// exports.ox_target.addEntity(netIds, options)
+const PEDS: Ped[] = [
+	{
+		model: "a_m_m_prolhost_01",
+		coords: [-252.2817, -297.2987, 21.6264, 69.0925],
+		scenario: "WORLD_HUMAN_CLIPBOARD"
+	}
+]
+
+setImmediate(() => {
+	PEDS.map(async ({ model, coords, scenario }) => {
+		const requestedModel = await requestModel(model)
+		if (!requestedModel) return
+	
+		const entity = CreatePed(0, requestedModel, coords[0], coords[1], coords[2] - 1.0, coords[3], false, true)
+		if (scenario) {
+			TaskStartScenarioInPlace(entity, scenario, 0, true)
+		}
+		
+		SetModelAsNoLongerNeeded(model)
+		FreezeEntityPosition(entity, true)
+		SetEntityInvincible(entity, true)
+		SetBlockingOfNonTemporaryEvents(entity, true)
+
+		exports.ox_target.addLocalEntity(entity, [
+			{
+				icon: 'fas fa-car',
+				label: 'Take a car',
+				onSelect: async () => {
+					const vehicle = await createVehicle('cypher', ...SPAWN_LOCATION)
+					TaskWarpPedIntoVehicle(cache.ped, vehicle.handle, -1)
+				}
+			}
+		])
+	})
+})
 
 addRadialItem([
   {
