7b3bc718727818a7a6767a5a7efb6e60b070fb15
Lenix <149199196+lehnihx@users.noreply.github.com>
2026-06-30 23:34:48 +0300

    #

--- resources/[standalone]/lenix_snipets/fxmanifest.lua
+++ resources/[standalone]/lenix_snipets/fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'gta5'
 
 dependencies {
-	''
+	'ox_lib'
 } 
 
 client_script 'dist/client.js'
--- resources/[standalone]/lenix_snipets/src/client/index.ts
+++ resources/[standalone]/lenix_snipets/src/client/index.ts
@@ -1,3 +1,5 @@
+import { addRadialItem, notify } from "@overextended/ox_lib/client"
+
 interface Ped {
 	model: string
 	coords: [number, number, number, number]
@@ -5,4 +7,27 @@ interface Ped {
 
 const PEDS: Ped[] = []
 
-exports.ox_target.addEntity(netIds, options)
+// exports.ox_target.addEntity(netIds, options)
+
+addRadialItem([
+  {
+    id: 'police',
+    label: 'Police',
+    icon: 'shield-halved',
+		onSelect: () => {
+			notify({
+				title: 'ابو داحم'
+			})
+		}
+  },
+  {
+    id: 'business_stuff',
+    label: 'Business',
+    icon: 'briefcase',
+    onSelect: () => {
+			notify({
+				title: 'Lenix!'
+			})
+    }
+  }
+])
