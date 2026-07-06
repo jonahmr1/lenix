603ad1a82f18cdf8ff63948a117c99b7ac4d2575
Lenix <149199196+lehnihx@users.noreply.github.com>
2026-07-06 13:39:45 +0300

    #

--- resources/[standalone]/lenix_snipets/fxmanifest.lua
+++ resources/[standalone]/lenix_snipets/fxmanifest.lua
@@ -2,19 +2,23 @@ fx_version 'cerulean'
 game 'gta5'
 
 dependencies {
-	'ox_lib'
+	'ox_lib',
+	'ox_core',
+	'ox_inventory',
+	'ox_target',
+	'pma-voice',
 }
 
-shared_script '@ox_lib/init.lua'
+shared_script '@ox_li/init.lua'
 client_scripts {
 	'dist/client.js',
-	'src/client/**/*.lua'
+	'src/client/**/*.lua',
 }
 server_scripts {
 	'dist/server.js',
 	'src/server/**/*.lua'
 }
 files {
-	'src/web/dist/**'
+	'src/web/dist/**',
 }
 ui_page	'src/web/dist/index.html'
