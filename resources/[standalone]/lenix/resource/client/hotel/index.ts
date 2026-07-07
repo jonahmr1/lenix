968c4e6ba3d8948a90b267478f5313ddc0fda05e
Lenix <149199196+lehnihx@users.noreply.github.com>
2026-07-07 23:02:24 +0300

    #

--- resources/[standalone]/lenix/resource/client/hotel/index.ts
+++ resources/[standalone]/lenix/resource/client/hotel/index.ts
@@ -1,12 +1,14 @@
 import { HOTEL_SAFES } from "common/hotel"
 
-const SAFE_SIZE = [0.66, 0.66, 0.66] as const
+const SAFE_SIZE = [1.66, 1.66, 1.66] as const
 
+console.debug(true)
 for (const [, { coords, rotation }] of Object.entries(HOTEL_SAFES)) {
   globalThis.exports.ox_target.addBoxZone({
     coords: coords,
 		size: SAFE_SIZE,
 		rotation: rotation,
+		debug: true,
     options: {
       label: 'Open Safe'
     }
