e4c7db83194f4f03fe6fbe0b9e0960657ca291a0
Lenix <149199196+lehnihx@users.noreply.github.com>
2026-07-07 18:03:56 +0300

    #

--- resources/[standalone]/lenix/web/vite.config.ts
+++ resources/[standalone]/lenix/web/vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
 		tailwindcss(),
     babel({ presets: [reactCompilerPreset()] })
   ],
+	base: './',
 	resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
