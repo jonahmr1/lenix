9f11ca9bd77bdf0b34e9b18027e938f03cb47644
Lenix <149199196+lehnihx@users.noreply.github.com>
2026-07-08 02:06:04 +0300

    #

--- resources/[standalone]/lenix/web/src/lib/index.ts
+++ resources/[standalone]/lenix/web/src/lib/index.ts
@@ -1,21 +1,7 @@
-export const emitCb = async <T = unknown>(id: string, data?: object): Promise<T> => {
-	try {
-		const response = await fetch(`https://${(window as any).GetParentResourceName()}/${id}`, {
-			method: 'post',
-			headers: {
-				'Content-Type': 'application/json; charset=UTF-8',
-			},
-			body: JSON.stringify(data ?? {})
-		})
-		return await response.json()
-	} catch(e) {
-		throw new Error(`Error occured while emiting a callback<${id}>. \n${e}`)
-	}
-}
-
 const cbs = new Map<string, (...params: unknown[]) => void>()
 
-window.addEventListener('message', (event: MessageEvent) => {
+// TODO: remove the event
+const handler = window.addEventListener('message', (event: MessageEvent) => {
 	const { id, params } = event.data
 	const cb = cbs.get(id)
 	if (!cb) throw new Error(`Callback<${id}> was does not exist yet`)
@@ -27,6 +13,21 @@ window.addEventListener('message', (event: MessageEvent) => {
 	}
 })
 
-export const onCb = (id: string, cb: (...params: unknown[]) => void) => {
-	cbs.set(id, cb)
+export type Callback<Id extends string, Params = unknown[]> = [Id, Params];
+
+export const onCb = <T extends [string, any[]]>(id: T[0], cb: (...params: T[1]) => void) => cbs.set(id, cb)
+
+export const emitCb = async <T = unknown>(id: string, data?: object): Promise<T> => {
+	try {
+		const response = await fetch(`https://${window.GetParentResourceName()}/${id}`, {
+			method: 'post',
+			headers: {
+				'Content-Type': 'application/json; charset=UTF-8',
+			},
+			body: JSON.stringify(data ?? {})
+		})
+		return await response.json()
+	} catch(e) {
+		throw new Error(`Error occured while emiting a callback<${id}>. \n${e}`)
+	}
 }
