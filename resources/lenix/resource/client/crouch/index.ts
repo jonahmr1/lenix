70492a990567b62a7dedc0e456f1fd6ee8a3b13b
Lenix <149199196+lehnihx@users.noreply.github.com>
2026-07-09 14:07:15 +0300

    #

--- resources/lenix/resource/client/crouch/index.ts
+++ resources/lenix/resource/client/crouch/index.ts
@@ -1,26 +1,20 @@
 import { addKeybind, cache, requestAnimSet } from '@overextended/ox_lib/client'
 
-AddStateBagChangeHandler(
-	'crouch',
-	`player:${cache.serverId}`,
-	async (_bagName, _key, value: boolean, _reserved, replicated) => {
-		if (replicated) return
+let crouched: false = false
 
-		await requestAnimSet('move_Ped_crouched')
-
-		if (!value) {
-			ResetPedMovementClipset(cache.ped, 1.0)
-			ResetPedWeaponMovementClipset(cache.ped)
-			ResetPedStrafeClipset(cache.ped)
-			SetPedStealthMovement(cache.ped, false, 'DEFAULT_ACTION')
-		} else {
-			SetPedMovementClipset(cache.ped, 'move_Ped_crouched', 1.0)
-			SetPedStrafeClipset(cache.ped, 'move_Ped_crouched_strafing')
-		}
-
-		RemoveAnimSet('move_ped_crouched')
-	},
-)
+const updateCrouchAnimation = async () => {
+	await requestAnimSet('move_Ped_crouched')
+	if (crouched) {
+		ResetPedMovementClipset(cache.ped, 1.0)
+		ResetPedWeaponMovementClipset(cache.ped)
+		ResetPedStrafeClipset(cache.ped)
+		SetPedStealthMovement(cache.ped, false, 'DEFAULT_ACTION')
+	} else {
+		SetPedMovementClipset(cache.ped, 'move_Ped_crouched', 1.0)
+		SetPedStrafeClipset(cache.ped, 'move_Ped_crouched_strafing')
+	}
+	RemoveAnimSet('move_ped_crouched')
+}
 
 addKeybind({
 	name: 'crouch',
@@ -29,6 +23,6 @@ addKeybind({
 	onReleased: () => {
 		if (cache.vehicle) return
 
-		LocalPlayer.state.set('crouch', !LocalPlayer.state.crouch, false)
+		updateCrouchAnimation()
 	},
 })
