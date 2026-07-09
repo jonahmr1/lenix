c6827c49bc56dffd7aebb799e9a0d57de4d6ec10
Lenix <149199196+lehnihx@users.noreply.github.com>
2026-07-09 14:51:06 +0300

    #

--- resources/lenix/resource/client/tick/index.ts
+++ resources/lenix/resource/client/tick/index.ts
@@ -6,19 +6,22 @@ import { setHudState } from '../hud'
 setTick(() => {
 	SetPedStealthMovement(cache.ped, false, 'nil')
 
+	// disable peds
 	SetParkedVehicleDensityMultiplierThisFrame(0.0)
 	SetVehicleDensityMultiplierThisFrame(0.0)
 	SetRandomVehicleDensityMultiplierThisFrame(0.0)
 	SetPedDensityMultiplierThisFrame(0.0)
 	SetScenarioPedDensityMultiplierThisFrame(0.0, 0.0)
 
+	// disable autoshuffle
+	SetPedConfigFlag(cache.ped, 184, true)
+
+	DisplayAmmoThisFrame(false)
+
 	displayIdentities()
 
 	setCuffs()
 
-	SetPedConfigFlag(cache.ped, 184, true)
-
-	DisplayAmmoThisFrame(false)
 
 	setHudState()
 })
