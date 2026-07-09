497f9767b04142f4a00fc040ea45a82b634e169a
Lenix <149199196+lehnihx@users.noreply.github.com>
2026-07-09 19:04:35 +0300

    #

--- resources/lenix/web/src/apps/roster/officer.tsx
+++ resources/lenix/web/src/apps/roster/officer.tsx
@@ -27,7 +27,7 @@ export const Officer = ({ callsign, name, duty_state, talk_state }: IOfficer) =>
 		</div>
 		<div className='flex-8 flex justify-between'>
 			<div className='text-white text-left'>{name}</div>
-			<Badge>
+			<Badge className='hidden'>{/* TODO */}
 				<AudioLines className={`${TalkStates[talk_state]}`} />
 			</Badge>
 		</div>
