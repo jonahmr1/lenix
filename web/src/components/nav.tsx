155f7775d7cf48f62cadcc806d90d59e5675a0eb
lenixdev <149199196+LenixDev@users.noreply.github.com>
2026-07-09 20:45:37 +0100

    feat(components): add key props to list items and hide active route in navigation

--- web/src/components/nav.tsx
+++ web/src/components/nav.tsx
@@ -1,23 +1,28 @@
 import { Menu } from "lucide-react";
 import { Button } from "./ui/button";
 import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem } from "./ui/dropdown-menu";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import type { Route } from "@/types";
 
 
-export const Nav = ({ routes }: { routes: Route[] }) => (
-	<nav className="sticky top-0">
-		<DropdownMenu>
-			<DropdownMenuTrigger className='m-4' render={<Button variant='ghost'><Menu /></Button>} />
-			<DropdownMenuContent className="w-40" align="start">
-				<DropdownMenuGroup>
-					{routes.map(({ path, label }) => (
-						<DropdownMenuItem>
-							<Link to={path}>{label}</Link>
-						</DropdownMenuItem>
-					))}
-				</DropdownMenuGroup>
-			</DropdownMenuContent>
-		</DropdownMenu>
-	</nav>
-)
+export const Nav = ({ routes }: { routes: Route[] }) => {
+	const { pathname } = useLocation()
+  const filteredRoutes = routes.filter(route => route.path !== pathname)
+
+	return (
+		<nav className="sticky top-0">
+			<DropdownMenu>
+				<DropdownMenuTrigger className='m-4' render={<Button variant='ghost'><Menu /></Button>} />
+				<DropdownMenuContent className="w-40" align="start">
+					<DropdownMenuGroup>
+						{filteredRoutes.map(({ path, label }) => (
+							<DropdownMenuItem key={path}>
+								<Link to={path}>{label}</Link>
+							</DropdownMenuItem>
+						))}
+					</DropdownMenuGroup>
+				</DropdownMenuContent>
+			</DropdownMenu>
+		</nav>
+	)
+}
