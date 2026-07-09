82b24a672090a5f570260d694deef02728d1c5f8
lenixdev <149199196+LenixDev@users.noreply.github.com>
2026-07-09 20:38:59 +0100

    #

--- web/src/types.ts
+++ web/src/types.ts
@@ -1,4 +1,11 @@
+import type { ReactNode } from "react"
+
 export interface Faq {
 	question: string
 	answer: string
+}
+export interface Route {
+	path: string
+	element: ReactNode
+	label: string
 }
