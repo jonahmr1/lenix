57440a9b1b2edda1244819d289994fb0b88b44f2
Lenix <149199196+lehnihx@users.noreply.github.com>
2026-07-09 14:46:41 +0300

    #

--- resources/[standalone]/minimal-hud/config/shared.lua
+++ resources/[standalone]/minimal-hud/config/shared.lua
@@ -3,7 +3,7 @@ return {
     speedUnit = "kph", -- Speed unit: "mph" or "kph".
 
     useBuiltInSeatbeltLogic = true, -- Enable custom seatbelt logic (true/false).
-    ejectMinSpeed = 20.0, -- Using built-in seatbelt logic: Minimum speed to eject when not wearing a seatbelt (in speedUnit).
+    ejectMinSpeed = 500.0, -- Using built-in seatbelt logic: Minimum speed to eject when not wearing a seatbelt (in speedUnit).
 
     minimapAlways = false, -- Always show minimap (true) or only in vehicles (false).
     compassAlways = false, -- Always show compass (true) or only in vehicles (false).
