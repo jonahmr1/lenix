f02c828877aae865d63790a89a9ad8a80b1e5a00
lenixdev <149199196+LenixDev@users.noreply.github.com>
2026-07-13 11:19:43 +0100

    chore: add JSX compiler options to deno.json and trim module examples in README and index.ts

--- modules/index.ts
+++ modules/index.ts
@@ -4,24 +4,9 @@
  *
  * @example
  * ```ts
- * import { wait } from '@lenix/lenix'
- *
- * await wait(1000)
- * ```
- * ```ts
  * import { entries } from '@lenix/lenix'
  *
  * const e = entries({ a: 1, b: 2 })
- * ```
- * ```ts
- * import { oneOf } from '@lenix/lenix'
- *
- * const valid = oneOf('a', ['a', 'b', 'c'])
- * ```
- * ```ts
- * import { raise } from '@lenix/lenix'
- *
- * const retVal = maybeNull ?? raise('Error occurred')
  * ```
  */
 
