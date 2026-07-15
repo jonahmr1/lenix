c3a642150acd0f9a23ebf7eb6af92a8e80855af6
lenixdev <149199196+LenixDev@users.noreply.github.com>
2026-07-15 02:19:36 +0100

    feat(app): add Discord icon link, switch to shadcn theme, and suppress hydration warnings

--- web/app/lib/layout.shared.tsx
+++ web/app/lib/layout.shared.tsx
@@ -1,9 +1,19 @@
 import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+import { RiDiscordFill } from '@remixicon/react'
 
 export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
       title: 'Code Hub',
     },
+		links: [
+      {
+        type: 'icon',
+        icon: <RiDiscordFill />,
+        text: 'Blog',
+        url: 'https://discord.gg/VgnvM6CzwG',
+				external: true
+      },
+    ],
   };
 }
