6b5bcf84e4c47f9b91c16c7a281ce1c2a31f83b3
lenixdev <149199196+LenixDev@users.noreply.github.com>
2026-07-16 17:15:12 +0100

    feat(entity): add overload to coords to optionally exclude heading and update .gitignore with package-preview

--- modules/client/entity.ts
+++ modules/client/entity.ts
@@ -4,10 +4,18 @@ import type { Vec3, Vec4 } from '../shared/types.ts'
 /**
  * Gets entity coordinates and heading.
  */
-const coords = (entity = PlayerPedId(), isAlive = true): Vec4 => [
-	...GetEntityCoords(entity, isAlive) as Vec3,
-	GetEntityHeading(entity)
-]
+function coords(excludeH: true, entity?: number, isAlive?: boolean): Vec3
+function coords(excludeH?: false, entity?: number, isAlive?: boolean): Vec4
+function coords(excludeH = false, entity = PlayerPedId(), isAlive = true): Vec3 | Vec4 {
+	const entityCoords = GetEntityCoords(entity, isAlive) as Vec3
+
+	if (excludeH) return entityCoords
+
+	return [
+		...entityCoords,
+		GetEntityHeading(entity)
+	]
+}
 
 export const getEntity = {
 	coords
