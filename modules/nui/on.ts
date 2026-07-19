bcbff18c3037639bd68e29220f8e62d6468d3da8
lenixdev <149199196+LenixDev@users.noreply.github.com>
2026-07-19 19:28:11 +0100

    refactor(nui/on): improve event handling and registration with logging and memory management

--- modules/nui/on.ts
+++ modules/nui/on.ts
@@ -1,17 +1,24 @@
 import type { Event } from '../shared/types.ts'
 
-const events = new Map<string, Set<(...params: unknown[]) => void>>()
+type Listener<T extends unknown[]> = {
+	callback: (...params: T) => void
+	log?: boolean
+}
+const events = new Map<string, Set<Listener<unknown[]>>>()
 
 // deno-lint-ignore no-window no-window-prefix no-unused-vars
 const handler = (event: MessageEvent) => {
 	const { id, params } = event.data
 	const callbacks = events.get(id)
-	if (!callbacks) return console.error(`Event<${id}> does not exist yet`)
+	if (!callbacks) return
 
-	try {
-		callbacks.forEach(callback => callback(...(params ?? [])))
-	} catch (e) {
-		throw new Error(`Error occured while receiving event<${id}>. \n${e}`)
+	for (const { callback, log } of callbacks) {
+		try {
+			callback(...(params ?? []))
+			log && console.info(`Callback from event<${id}> was called`)
+		} catch (e) {
+			console.error(`Error occured while receiving event<${id}>. \n${e}`)
+		}
 	}
 }
 
@@ -22,19 +29,30 @@ export const onEvent = <
 	T extends Event<string, unknown[]>
 >(
 	id: T[0],
-	cb: (...params: T[1]) => void
+	cb: (...params: T[1]) => void,
+	listen?: boolean
 ): () => void => {
-	if (events.size === 0) window.addEventListener('message', handler)
+	if (events.size === 0) {
+		window.addEventListener('message', handler)
+		listen && console.info(`Event<${id}> is the first on the window`)
+	}
 
-	const callbacks = events.get(id) ?? new Set<(...params: T[1]) => void>()
-	callbacks.add(cb)
+	const callbacks = events.get(id) ?? new Set<Listener<unknown[]>>()
+	const listenerCb = { callback: cb, log: listen }
+	callbacks.add(listenerCb)
 	events.set(id, callbacks)
+	listen && console.info(`Event<${id}> was registered`)
 
 	return () => {
-		callbacks.delete(cb)
-		if (callbacks.size === 0) events.delete(id)
+		listen && console.info(`Event<${id}> was unregistered`)
+		callbacks.delete(listenerCb)
+		if (callbacks.size === 0) {
+			events.delete(id)
+			listen && console.info(`Event<${id}> got no callbacks left, memory cleared successfully`)
+		}
 		if (events.size === 0) {
 			window.removeEventListener('message', handler)
+			listen && console.info(`No events left, this event<${id}> was the last, opting out the listen...`)
 		}
 	}
 }
