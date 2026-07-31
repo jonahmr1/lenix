efc430f075c07a27c50690c31fb96af34ec22275
lenixdev <149199196+LenixDev@users.noreply.github.com>
2026-07-31 16:52:29 +0100

    feat(roster): add MAX_CALLSIGN_LENGTH constant and update script type definitions

--- lenixm/lenix/types/dirs.d.ts
+++ lenixm/lenix/types/dirs.d.ts
@@ -2,5 +2,3 @@
 
 export type ClientScript = 'crouch' | 'cuffs' | 'elevators' | 'escort' | 'groups' | 'handsup' | 'hotel' | 'hud' | 'identities' | 'interactions' | 'medical' | 'prison' | 'radialmenu' | 'radio' | 'rent' | 'robbery' | 'roster' | 'safezones' | 'settings' | 'tick' | 'weather'
 export type ServerScript = 'appearance' | 'cuffs' | 'db' | 'escort' | 'groups' | 'hotel' | 'interactions' | 'medical' | 'prison' | 'robbery' | 'roster'
-export type CommonScript = 'hotel' | 'prison' | 'robbery' | 'roster'
-export type Script = 'appearance' | 'crouch' | 'cuffs' | 'db' | 'elevators' | 'escort' | 'groups' | 'handsup' | 'hotel' | 'hud' | 'identities' | 'interactions' | 'medical' | 'prison' | 'radialmenu' | 'radio' | 'rent' | 'robbery' | 'roster' | 'safezones' | 'settings' | 'tick' | 'weather'
