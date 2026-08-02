90e19c253ba2cba46d03783a640f289ececcb5a8
lenixdev <149199196+LenixDev@users.noreply.github.com>
2026-08-02 17:40:26 +0100

    fix(components): remove unnecessary return statement and add missing import statements

--- web/apps/portfolio/src/components/articles/footer.tsx
+++ web/apps/portfolio/src/components/articles/footer.tsx
@@ -8,26 +8,24 @@ export const Footer = ({ className, items }: {
 		url: string
 		replace?: boolean
 	}[]
-}) => {
-	return (
-		<div className={`border-t border-foreground/10 flex items-center justify-between gap-10 ${className}`}>
-			<p className='text-[11px] text-foreground/20'>
-				© {new Date().getFullYear()} Lenix
-			</p>
-			<div className='flex gap-4'>
-				{items.map(({ label, url, replace }) => (
-					<Link
-						to={url}
-						key={label}
-						target={replace === true ? '_self' : '_blank'}
-						rel='noopener noreferrer'
-						className='text-[11px] tracking-[2px] uppercase text-foreground/20 hover:text-foreground transition-colors group flex items-center gap-1'
-					>
-						{label}
-						{!replace && <MoveUpRight size={15} className='opacity-0 group-hover:opacity-100' />}
-					</Link>
-				))}
-			</div>
+}) => (
+	<div className={`border-t border-foreground/10 flex items-center justify-between gap-10 ${className}`}>
+		<p className='text-[11px] text-foreground/20'>
+			© {new Date().getFullYear()} Lenix
+		</p>
+		<div className='flex gap-4'>
+			{items.map(({ label, url, replace }) => (
+				<Link
+					to={url}
+					key={label}
+					target={replace === true ? '_self' : '_blank'}
+					rel='noopener noreferrer'
+					className='text-[11px] tracking-[2px] uppercase text-foreground/20 hover:text-foreground transition-colors group flex items-center gap-1'
+				>
+					{label}
+					{!replace && <MoveUpRight size={15} className='opacity-0 group-hover:opacity-100' />}
+				</Link>
+			))}
 		</div>
-	)
-}
+	</div>
+)
--- web/apps/portfolio/src/pages/main.tsx
+++ web/apps/portfolio/src/pages/main.tsx
@@ -6,36 +6,46 @@ import { Footer } from '@/components/articles/footer'
 import { Quotes } from '@/components/articles/quotes'
 import { DataProvider } from '@/lib/context'
 import { Timeline } from '@/components/articles/timeline'
+import { Button } from '@workspace/ui/components/button'
+import { ExternalLink } from 'lucide-react'
+import { useNavigate } from 'react-router'
 
-export const Main = () => (
-	<DataProvider>
-		<div className='w-full flex justify-evenly bg-background text-foreground'>
-			<div className='landscape:w-2/3 portrait:w-6/7 h-full flex flex-col gap-20'>
-				<div className='h-screen flex flex-col justify-between'>
-					<div className='min-h-5/10 flex items-end'>
-						<Introduc />
+export const Main = () => {
+	const nav = useNavigate()
+	return (
+		<DataProvider>
+			<div className='w-full flex justify-evenly bg-background text-foreground'>
+				<div className='landscape:w-2/3 portrait:w-6/7 h-full flex flex-col gap-20'>
+					<div className='h-screen flex flex-col justify-between'>
+						<div className='min-h-5/10 flex items-end'>
+							<Introduc />
+						</div>
+						<div className='h-full flex items-center justify-center gap-2'>
+							<Button variant='outline' onClick={() => nav('/contact')}>Contact Me</Button>
+							<Button onClick={() => window.open('https://business.lenix.dev', '_blank')}>Explore Business <ExternalLink /></Button>
+						</div>
+					</div>
+					<div className='min-h-screen flex flex-col justify-evenly'>
+						<Stats />
+						<Activity />
+						<Langs />
+					</div>
+					<Timeline />
+					<div>
+						<Quotes />
+					</div>
+					<div className='mt-12'>
+						<Footer items={[
+							{ label: 'GitHub', url: 'https://github.com/jonahmr1' },
+							{
+								label: 'Contact',
+								url: '/contact',
+								replace: true,
+							},
+						]} />
 					</div>
 				</div>
-				<div className='min-h-screen flex flex-col justify-evenly'>
-					<Stats />
-					<Activity />
-					<Langs />
-				</div>
-				<Timeline />
-				<div>
-					<Quotes />
-				</div>
-				<div className='mt-12'>
-					<Footer items={[
-						{ label: 'GitHub', url: 'https://github.com/jonahmr1' },
-						{
-							label: 'Contact',
-							url: '/contact',
-							replace: true,
-						},
-					]} />
-				</div>
 			</div>
-		</div>
-	</DataProvider>
-)
+		</DataProvider>
+	)
+}
