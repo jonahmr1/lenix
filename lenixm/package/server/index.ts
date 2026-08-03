247ab3b279ba7d0ce7b072f1bbc9bbafa9fa5d14
lenixdev <149199196+LenixDev@users.noreply.github.com>
2026-08-03 17:51:53 +0100

    refactor(shared): reexport api from client and server packages

--- lenixm/package/server/index.ts
+++ lenixm/package/server/index.ts
@@ -5,3 +5,4 @@
  */
 
 export * from './server'
+export * from '../shared/api'
--- lenixm/package/shared/index.ts
+++ lenixm/package/shared/index.ts
@@ -4,7 +4,6 @@
  * Shared helpers that can be used from both client and server contexts.
  */
 
-export * from './api.ts'
 export * from './types.ts'
 export * from './repeat.ts'
 export * from './pallete.ts'
