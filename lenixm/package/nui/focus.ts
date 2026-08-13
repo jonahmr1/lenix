40ac35ce286b5709e06511d0d85f2b12c38281af
lenixdev <149199196+LenixDev@users.noreply.github.com>
2026-08-13 13:06:51 +0100

    refactor(nui/focus): rename focus to setFocus and simplify focus/unFocus API

--- lenixm/package/nui/focus.ts
+++ lenixm/package/nui/focus.ts
@@ -2,26 +2,15 @@ import { Request } from '../shared/types.ts'
 import { fetchNui } from './fetch.ts'
 
 /**
- * Requests keyboard and cursor focus for the NUI browser.
+ * Sets the keyboard and cursor focus state for the NUI browser.
  */
-export const focus = (exclude?: 'keyboard' | 'cursor'): Promise<true> => {
-	return fetchNui<Request<true, '__nuiFocus', {
-		keyboard: boolean,
-		cursor: boolean
-	}>>('__nuiFocus', { keyboard: exclude === 'keyboard' ? false : true, cursor: exclude === 'cursor' ? false : true })
-}
+export const setFocus = (
+	keyboard: boolean,
+	cursor: boolean
+) => fetchNui<Request<true, '__nuiFocus', {
+	keyboard: boolean,
+	cursor: boolean
+}>>('__nuiFocus', { keyboard, cursor })
 
-export const unFocus = (
-	key: KeyboardEvent['key'],
-	onEvent: () => void
-) => {
-	
-	const handler = (e: KeyboardEvent) => {
-	 if (e.key !== key) return
-
-	 onEvent()
-	}
-	
-	window.addEventListener('keydown', handler)
-	return () => window.removeEventListener('keydown', handler)
-}
+export const focus = () => setFocus(true, true);
+export const unFocus = () => setFocus(false, false);
