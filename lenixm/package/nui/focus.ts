8bd89f8be20accfd4f8c94dde4a1e1418d3ed8b7
lenixdev <149199196+LenixDev@users.noreply.github.com>
2026-08-13 12:44:15 +0100

    feat(nui): add unFocus helper and update focus to accept exclusion

--- lenixm/package/nui/focus.ts
+++ lenixm/package/nui/focus.ts
@@ -1,10 +1,27 @@
-import type { _InternalRequests } from '../shared/types.ts'
-import { triggerNui } from './trigger.ts'
+import { Request } from '../shared/types.ts'
+import { fetchNui } from './fetch.ts'
 
 /**
  * Requests keyboard and cursor focus for the NUI browser.
  */
-export const focus = (
-	keyboard = true,
-	cursor = true
-): Promise<true> => triggerNui<_InternalRequests['focus']>('__nuiFocus', { keyboard, cursor })
+export const focus = (exclude?: 'keyboard' | 'cursor'): Promise<true> => {
+	return fetchNui<Request<true, '__nuiFocus', {
+		keyboard: boolean,
+		cursor: boolean
+	}>>('__nuiFocus', { keyboard: exclude === 'keyboard' ? false : true, cursor: exclude === 'cursor' ? false : true })
+}
+
+export const unFocus = (
+	key: KeyboardEvent['key'],
+	onEvent: () => void
+) => {
+	
+	const handler = (e: KeyboardEvent) => {
+	 if (e.key !== key) return
+
+	 onEvent()
+	}
+	
+	window.addEventListener('keydown', handler)
+	return () => window.removeEventListener('keydown', handler)
+}
