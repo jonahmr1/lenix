093a16397e67872ed1adc403a7aa4dbb2dae9a44
lenixdev <149199196+LenixDev@users.noreply.github.com>
2026-08-16 15:09:47 +0100

    feat(server): add teleport utility, rename handleFromSource to handleBySource, and bump version to 1.8.3

--- lenixm/package/server/server.ts
+++ lenixm/package/server/server.ts
@@ -1,11 +1,20 @@
-export const server: {
-	entity: {
-		handle: (netId: number) => number;
-		handleFromSource: (source: number) => number;
-	};
-} = {
+export const server = {
 	entity: {
 		handle: (netId: number) => NetworkGetEntityFromNetworkId(netId),
-		handleFromSource: (source: number) => GetPlayerPed(source.toString())
+		handleBySource: (source: number) => GetPlayerPed(source.toString()),
+		teleport: (
+			entity: number,
+			x: number,
+			y: number,
+			z: number,
+			h?: number,
+			clearArea = false,
+			alive = true,
+			deadDisable = false,
+			ragdol = false
+		) => {
+			SetEntityCoords(entity, x, y, z, alive, deadDisable, ragdol, clearArea)
+			h && SetEntityHeading(entity, h)
+		},
 	}
 }
