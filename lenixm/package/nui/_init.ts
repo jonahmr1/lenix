329cdea851c046d7119070530d3391834ad812a1
lenixdev <149199196+LenixDev@users.noreply.github.com>
2026-08-26 16:28:02 +0100

    refactor(nui): use type-only import for Request

--- lenixm/package/nui/_init.ts
+++ lenixm/package/nui/_init.ts
@@ -1,4 +1,4 @@
-import { Request } from '../shared/types'
+import type { Request } from '../shared/types'
 import { fetchNui } from './fetch'
 
 fetchNui<Request<null, '__nuiInit'>>('__nuiInit', {})
