46c9a3ab40bfab1f10c509e3c58f68ea5edfc539
lenixdev <149199196+LenixDev@users.noreply.github.com>
2026-08-28 19:49:30 +0100

    refactor(emotes): replace hardcoded animation strings with constant and clean up unused imports and hands‑up logic

--- lenixm/lenix/resource/client/emotes/index.ts
+++ lenixm/lenix/resource/client/emotes/index.ts
@@ -1,13 +1,14 @@
-import { cache, checkDependency, requestAnimDict, requestAnimSet } from '@overextended/ox_lib/client'
-import { control, entity } from 'lenix/client'
+import { cache, checkDependency, requestAnimSet } from '@overextended/ox_lib/client'
+import { control } from 'lenix/client'
 
 checkDependency('ox_lib', '3.39.0', true)
 
+const animDict = 'move_Ped_crouched'
+
 let crouched: boolean = false
-let handsUp: boolean = false
 
 const updateCrouchAnimation = async () => {
-	await requestAnimSet('move_Ped_crouched')
+	await requestAnimSet(animDict)
 
 	if (crouched) {
 		ResetPedMovementClipset(cache.ped, 1.0)
@@ -15,11 +16,11 @@ const updateCrouchAnimation = async () => {
 		ResetPedStrafeClipset(cache.ped)
 		SetPedStealthMovement(cache.ped, false, 'DEFAULT_ACTION')
 	} else {
-		SetPedMovementClipset(cache.ped, 'move_Ped_crouched', 1.0)
+		SetPedMovementClipset(cache.ped, animDict, 1.0)
 		SetPedStrafeClipset(cache.ped, 'move_Ped_crouched_strafing')
 	}
 	
-	RemoveAnimSet('move_ped_crouched')
+	RemoveAnimSet(animDict)
 	crouched = !crouched
 }
 
@@ -27,16 +28,4 @@ control.on({
 	event: 'press',
 	key: 'LCONTROL',
 	onEvent: () => !cache.vehicle && updateCrouchAnimation(),
-})
-
-control.on({
-	event: 'press',
-	key: 'X',
-	onEvent: async () => {
-		if (Player(cache.ped).state.isCuffed) return
-
-		await requestAnimDict('missminuteman_1ig_2')
-		handsUp ? ClearPedTasks(cache.ped) : await entity.playAnim(cache.ped, 'missminuteman_1ig_2', 'handsup_base')
-		handsUp = !handsUp
-	},
 })
