65d2682b5e157688ec5c62969691a2613e3d6330
lenixdev <149199196+LenixDev@users.noreply.github.com>
2026-09-20 20:17:50 +0100

    style(app): update heading font variable and adjust border radius value

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -6,6 +6,8 @@ import { RootProvider } from 'fumadocs-ui/provider/next';
 
 import { Source_Sans_3, Oxanium, Geist_Mono } from "next/font/google"
 
+const oxaniumHeading = Oxanium({subsets:['latin'],variable:'--font-heading'});
+
 const sourceSans3 = Source_Sans_3({
   subsets: ["latin"],
   variable: "--font-source-sans-3",
@@ -30,7 +32,7 @@ export default function RootLayout({
     <html
       lang="en"
       suppressHydrationWarning
-      className={cn(sourceSans3.variable, oxanium.variable, geistMono.variable)}
+      className={cn(sourceSans3.variable, oxanium.variable, geistMono.variable, oxaniumHeading.variable)}
     >
       <body className="flex flex-col min-h-screen typeset typeset-docs">
         <ThemeProvider>
