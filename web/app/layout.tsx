8988b77fcb8db27ea31c2057b3688898bdc3be31
lenixdev <149199196+LenixDev@users.noreply.github.com>
2026-09-20 18:47:57 +0100

    style(fonts): replace Outfit with Source Sans 3 in globals.css and layout.tsx

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,14 +1,14 @@
-import { Geist_Mono, Outfit, Oxanium } from "next/font/google"
-
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { cn } from "@/lib/utils";
 import { Toaster } from "@/components/ui/sonner";
 import { RootProvider } from 'fumadocs-ui/provider/next';
 
-const outfit = Outfit({
+import { Source_Sans_3, Oxanium, Geist_Mono } from "next/font/google"
+
+const sourceSans3 = Source_Sans_3({
   subsets: ["latin"],
-  variable: "--font-outfit",
+  variable: "--font-source-sans-3",
 })
 
 const oxanium = Oxanium({
@@ -30,7 +30,7 @@ export default function RootLayout({
     <html
       lang="en"
       suppressHydrationWarning
-      className={cn(outfit.variable, oxanium.variable, geistMono.variable)}
+      className={cn(sourceSans3.variable, oxanium.variable, geistMono.variable)}
     >
       <body className="flex flex-col min-h-screen typeset typeset-docs">
         <ThemeProvider>
