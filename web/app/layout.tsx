6a01a881432b3bce91b7bd19a496e589d7d68dc9
lenixdev <149199196+LenixDev@users.noreply.github.com>
2026-09-24 02:06:16 +0100

    chore(app): update fumadocs import and add RootProvider theme

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -46,7 +46,9 @@ export default function RootLayout({
     >
       <body className="flex flex-col min-h-screen typeset typeset-docs">
         <ThemeProvider>
-					<RootProvider>
+					<RootProvider
+						theme={{ hotKey: false }}
+					>
 						<TooltipProvider>
 							{children}
 							<Analytics />
